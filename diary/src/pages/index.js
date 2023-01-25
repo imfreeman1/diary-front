57176b8d5e35ee31b513899a51badb5618104f3b
KUD <kimud6003@gmail.com>
2023-01-25 23:08:18 +0900

    - Feature : index.js
    -  landing 페이지로 라우팅

--- diary/src/pages/index.js
+++ diary/src/pages/index.js
@@ -1,7 +1,9 @@
+import React from 'react';
+import Landing from './Landing';
+// import { googleLogout } from '@react-oauth/google';
+
 export default function Home() {
   return (
-    <>
-      <div className='text-red-800'> hello world</div>
-    </>
+    <Landing />
   );
 }
