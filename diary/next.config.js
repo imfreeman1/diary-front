2331be9f08183384df83efaf1c4542943977c1bb
KUD <kimud6003@gmail.com>
2023-01-25 23:07:21 +0900

    - Feature : next.config.js
    -  google API 토큰 env화

--- diary/next.config.js
+++ diary/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env:{
+    GOOGLE_KEY: process.env.REACT_APP_GOOGLE_API_KEY
+  }
 }
 
 module.exports = nextConfig
