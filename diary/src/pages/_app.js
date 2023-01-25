458cd2b536cd41adc17b602ca0026d681aae8214
KUD <kimud6003@gmail.com>
2023-01-25 23:05:33 +0900

    - Feature : _app.js
    - GoogleOAuthProvider 설정
    - env를 통한 APIkey 호출

--- diary/src/pages/_app.js
+++ diary/src/pages/_app.js
@@ -1,5 +1,10 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <GoogleOAuthProvider clientId={process.env.GOOGLE_KEY}>
+      <Component {...pageProps} />
+    </GoogleOAuthProvider>
+  );
 }
