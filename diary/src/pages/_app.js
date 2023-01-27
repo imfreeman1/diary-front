866e9f6e74d76c63a7f1f0827dca1eedabc7f6dd
KUD <kimud6003@gmail.com>
2023-01-27 13:47:14 +0900

    - Feature _app.js
    - 6라인 Readux Provider
    - 7라인 CookiesProvider
    - 16라인 prop-type 작성

--- diary/src/pages/_app.js
+++ diary/src/pages/_app.js
@@ -1,10 +1,24 @@
 import '@/styles/globals.css';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import PropTypes from 'prop-types';
+import { Provider } from 'react-redux';
+import { CookiesProvider } from 'react-cookie';
 
-export default function App({ Component, pageProps }) {
+function App({ Component, pageProps }) {
   return (
-    <GoogleOAuthProvider clientId={process.env.GOOGLE_KEY}>
-      <Component {...pageProps} />
-    </GoogleOAuthProvider>
-  );
+    <Provider store={store}>
+      <CookiesProvider>
+        <GoogleOAuthProvider clientId={process.env.GOOGLE_KEY}>
+          <Component {...pageProps} />
+        </GoogleOAuthProvider>
+      </CookiesProvider>
+    </Provider>
+  );// 이창수 바보
 }
+
+App.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.shape({}).isRequired,
+};
+
+export default App;
