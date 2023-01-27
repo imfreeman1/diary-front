c3d4d367c0ff07d9a3865e7108db78bcff6759c0
eunji1 <ssak1088@gmail.com>
2023-01-27 18:39:35 +0900

    Feature : index.js - 쿠키 정보가 있으면 landing-> Main 화면 전환

--- diary/src/pages/index.js
+++ diary/src/pages/index.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import Landing from './Landing';
+import React, { useEffect } from "react";
+import Landing from "./Landing";
+import { useSelector } from "react-redux";
+import { useCookies } from "react-cookie";
+import Main from "./Main";
+
 // import { googleLogout } from '@react-oauth/google';
 
 export default function Home() {
-  return (
-    <Landing />
-  );
+  const [{ token }] = useCookies(["token"]);
+  // const userInfo = useSelector((state) => state);
+  // useEffect(() => {
+  //   console.log(token);
+  // }, []);
+
+  return <>{token ? <Main /> : <Landing />}</>;
 }
