12c27a488fc62c12cbc54e8ed95de7c5351f6add
eunji1 <ssak1088@gmail.com>
2023-01-27 22:31:20 +0900

    FIX : index.js React Hydration Error :

--- diary/src/pages/index.js
+++ diary/src/pages/index.js
@@ -3,15 +3,19 @@ import Landing from "./Landing";
 import { useSelector } from "react-redux";
 import { useCookies } from "react-cookie";
 import Main from "./Main";
-
 // import { googleLogout } from '@react-oauth/google';
 
 export default function Home() {
   const [{ token }] = useCookies(["token"]);
-  // const userInfo = useSelector((state) => state);
-  // useEffect(() => {
-  //   console.log(token);
-  // }, []);
+
+  const [hydrated, setHydrated] = React.useState(false);
+
+  useEffect(() => {
+    setHydrated(true);
+  }, []);
+  if (!hydrated) {
+    return null;
+  }
 
   return <>{token ? <Main /> : <Landing />}</>;
 }
