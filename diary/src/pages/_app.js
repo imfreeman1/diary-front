21ddad06c3f4ca0401282bcadcd39c2914ea5d09
KUD <kimud6003@gmail.com>
2023-01-27 13:55:34 +0900

    -HotFix _app.js
    - store redux폴더에서 import

--- diary/src/pages/_app.js
+++ diary/src/pages/_app.js
@@ -3,6 +3,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
 import { CookiesProvider } from 'react-cookie';
+import { store } from '../Redux/store';
 
 function App({ Component, pageProps }) {
   return (
