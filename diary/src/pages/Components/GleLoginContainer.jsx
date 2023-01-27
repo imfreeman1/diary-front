2e75aa25ab7d2950ee75158cc390ed65094ac505
KUD <kimud6003@gmail.com>
2023-01-27 13:52:02 +0900

    - Feature GleLoginContainer.jsx
    - rtk로 userInfo 저장
    - reactCookie를 통해 token 저장

--- diary/src/pages/Components/GleLoginContainer.jsx
+++ diary/src/pages/Components/GleLoginContainer.jsx
@@ -1,14 +1,31 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import { useGoogleLogin } from '@react-oauth/google';
-import React, { useState } from 'react';
-import { getInfo } from '../api/googleAPI';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useCookies } from 'react-cookie';
+import getInfo from '../api/googleAPI';
 import GleLoginPresenter from './GleLoginPresenter';
+import { setAuth } from '@/Redux/action';
 
 function GleLoginContainer() {
-  const [userInfo, setUserInfo] = useState();
+  const dispatch = useDispatch();
+  const userInfo = useSelector((state) => state.userInfo);
+  const [{ token }, setCookie] = useCookies(['token']);
+
+  const setUserInfoCookies = (data) => {
+    setCookie('token', data, { path: '/' });
+  };
+
+  const setUserInfo = (data) => {
+    dispatch(setAuth({ data, setUserInfoCookies }));
+  };
+
   const loginLogic = useGoogleLogin({
-    onSuccess: (tokenResponse) => getInfo(tokenResponse.access_token, setUserInfo),
+    onSuccess: (tokenResponse) => {
+      getInfo(tokenResponse.access_token, setUserInfo);
+    },
   });
+
   return (
     <GleLoginPresenter onclick={loginLogic} />
   );
