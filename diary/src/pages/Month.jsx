84ecadd7289809c044bf0f3a2a5e01a6fd684633
eunji1 <ssak1088@gmail.com>
2023-01-29 23:08:11 +0900

    - Feature : Month getCalendar -> {locdate : 1,  day : "Sun",  isHoliday: true}

--- diary/src/pages/Month.jsx
+++ diary/src/pages/Month.jsx
@@ -1,32 +1,46 @@
 import React from 'react'
 import MonthWeek from './Month/MonthWeek';
 
-const days = ["Sun","Sat","Mon","Tue","Wed","Thu","Fri"];
-const monthTable = Array.from(Array(5),(val)=>{
-  let arr = [];
-  for (let i of days) {
-    let obj = {};
-    obj.day = i;
-    obj.isHoliday = i==="Sun"? true: false
-    
-    arr.push(obj);
-  }
-  return arr;
-});
-const date = new Date()
+const getCalendar = (year, month) =>{
+    // 윤달
+    const monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    const dayOfTheWeek = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+    const monthStartDay = new Date(year, month, 1).getDay()
 
+    const makeMonthTable = Array.from(Array(5),(_, idx)=>{
+      let week = [];
+      let startday = 1;
+      for (let i of dayOfTheWeek) {
+        let dateObj = {};
+        let date = (idx*7)-monthStartDay+startday++
+        if(date>monthDays[month]||date<1) date = undefined
+        dateObj.locdate = date;
+        dateObj.day = i;
+        dateObj.isHoliday = i==="Sun"? true: false
+        dateObj.dateName = ""
+        week.push(dateObj);
+      }
+      return week;
+    });
+
+    return makeMonthTable
+    
+}
+// date.getFullYear(); date.getMonth() + 1; date.getDate();
+const dayOfTheWeek = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
 
 const MonthPage = () => {
   // getRestDeInfo("2021", "02")
-  console.log(date)
-  console.log(monthTable);
+  let monthTable = getCalendar(2023,1)
+  console.log(monthTable)
   return (
     <div className='flex justify-center pt-5 h-full w-full bg-orange-200'>
       <div className='bg-white'>
-        <div className='text-4xl border-black border-2 rounded-[50%] w-min px-6'>5</div>
-        <div className='flex mb-2 mt-4'>{days.map((val, idx)=> {
-          return val==="Sun"?<div className='w-32 flex justify-center text-[#FF0000]' key={idx}>{val}</div>:<div className='w-32 flex justify-center' key={idx}>{val}</div>
-        })}
+        <div className='text-4xl border-black border-2 rounded-[50%] w-min px-6'>2</div>
+        <div className='flex mb-2 mt-4'>
+          {dayOfTheWeek.map((val, idx)=> 
+            <div className={`w-32 flex justify-center ${val==="Sun"? "text-[#FF0000]":""}`} key={idx}>{val}</div>
+        )}
         </div>
         <table className='border-collapse border border-black'>
           {monthTable.map((val,idx) => {
