1dc9fa6f24f5c6b6b32b272857319be8e5607620
imfreeman1 <dkfrhkt@gmail.com>
2023-01-30 16:22:32 +0900

    Month 수정
     - 휴일 판별 후 해당 태그의 글자 색상 변경 방식 수정

--- diary/src/pages/Month/MonthDate.jsx
+++ diary/src/pages/Month/MonthDate.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MonthDate = ({isHoliday,day,key}) => {
   return (
-    isHoliday?<td className='border w-32 h-32 text-[#FF0000] border-black' key={key}>{day}</td> : <td className='border border-black w-32 h-32' key={key}>{day}</td>
+    <td className={`border w-32 h-32  border-black ${isHoliday? 'text-[#FF0000]' : null}`} key={key}>{day}</td>
   )
 }
 
