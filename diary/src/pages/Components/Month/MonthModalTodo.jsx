48928c396717df675fe37ccfcbc7423677b80030
eunji1 <ssak1088@gmail.com>
2023-02-02 23:29:43 +0900

    - Feature : MonthModal Month 날짜를 더블클릭하면 모달창 나오게 & 모달창에 투두 리스트 구현

---
+++ diary/src/pages/Components/Month/MonthModalTodo.jsx
@@ -0,0 +1,47 @@
+import { delTodo, setTodo } from '@/Redux/action';
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import Button from '../Button';
+const MonthModalTodo = ({dayInfo, handleModalClose}) => {
+
+    const dispatch = useDispatch();
+    const [text, setText] = useState("")
+    
+    const handleChange = (e) => {
+      setText(e.target.value)
+    };
+  
+    const handleKeyPress = (e) => {
+        e.preventDefault()
+        dispatch(setTodo(text))
+        setText("");
+      }
+  
+    const onDelete = (todo) => {
+      dispatch(delTodo(todo));
+      console.log(todo)
+  
+    };
+    const {todos} = useSelector((state)=>state.todoReducer)
+    console.log(todos)
+  return (
+    <div className='absolute inset-x-0 w-96 h-96 mx-auto bg-gray-200 text-center select-none'>
+        <div className='text-xl text-red-400 font-bold'>{dayInfo.locdate}</div>
+      <div>todo
+        <form  onSubmit={handleKeyPress}>
+          <input placeholder='todo 입력하세요' value={text} onChange={handleChange} ></input>
+        </form>
+        <div className='text-left m-5 w-48 h-48 bg-white mx-auto'>
+          {todos.map((todo, idx)=>(
+            <li key={idx}>{todo.text}<Button onClick={()=>onDelete(todo)} content="DELETE"></Button></li>
+          ))}
+        </div>
+        <div>
+          <Button onClick={()=>handleModalClose()} content="CLOSE" />
+        </div> 
+      </div>
+    </div>
+  )
+}
+
+export default MonthModalTodo
