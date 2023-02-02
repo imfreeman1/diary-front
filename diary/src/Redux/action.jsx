070c5d00908d9559388d3bf9111cf3f4d08bd84d
eunji1 <ssak1088@gmail.com>
2023-02-02 23:30:06 +0900

    - Feature : reducer todoreducer

--- diary/src/Redux/action.jsx
+++ diary/src/Redux/action.jsx
@@ -1,6 +1,9 @@
 import { googleAuthSlice } from './googleAuthSlice';
 import { calendarSlice } from './calendarSlice';
+import { todoSlice } from './todoSlice';
 
 export const { setAuth, removeAuth } = googleAuthSlice.actions;
 
 export const { setCal } = calendarSlice.actions;
+
+export const { setTodo, delTodo } = todoSlice.actions;
--- diary/src/Redux/store.jsx
+++ diary/src/Redux/store.jsx
@@ -1,8 +1,9 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { googleAuthReducer } from './googleAuthSlice';
 import { calendarReducer } from './calendarSlice';
+import { todoReducer } from './todoSlice';
 
-const rootReducer = combineReducers({googleAuthReducer, calendarReducer})
+const rootReducer = combineReducers({googleAuthReducer, calendarReducer, todoReducer})
 
 export const store = configureStore({
   reducer: rootReducer,
