2564db71a92909b6238a212ee0add6c751aea5f3
eunji1 <ssak1088@gmail.com>
2023-02-05 20:03:39 +0900

    - MonthModal파일 삭제 하고 InputModal과 EditModal로 나눔

--- diary/src/pages/Components/Month/MonthDate.jsx
+++ diary/src/pages/Components/Month/MonthDate.jsx
@@ -1,21 +1,48 @@
-import MonthModal from '@/pages/Components/Month/MonthModal'
+import MonthInputModal from '@/pages/Components/MonthModal/MonthInputModal'
 import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
+import MonthEditModal from '../MonthModal/MonthEditModal'
 
 const MonthDate = ({dayInfo}) => {
   const [visible, setVisible] = useState(false)
+  const [itemVisible, setItemVisible] = useState(false)
 
   const handleModalOpen = () =>{
       setTimeout(() => {
         setVisible(true)
       }, 300);
   }
+  const handleItemModalOpen = () =>{
+    setTimeout(() => {
+      setItemVisible(true)
+    }, 300);
+}
   const handleModalClose = () =>{
     setVisible(false)
   }
+  const handleItemModalClose = () =>{
+    setItemVisible(false)
+  }
+  const {locdate} = dayInfo
+  const {todos} = useSelector((state)=>state.todoReducer)
+  console.log("todos", todos)
   return (
     <>
-    <td onDoubleClick={()=>handleModalOpen()} className={`border w-32 h-32 border-black font-bold text-lg ${dayInfo.isInMonth?dayInfo.isHoliday?"text-[#FF0000]":"text-black":"text-gray-400"}`}>{dayInfo.date} {dayInfo.dateName}</td>
-    <MonthModal dayInfo={dayInfo} visible={visible} handleModalClose={handleModalClose}></MonthModal>
+    <td onDoubleClick={()=>handleModalOpen()} className={`static border w-32 h-32 border-black font-bold text-lg ${dayInfo.isInMonth?dayInfo.isHoliday?"text-[#FF0000]":"text-black":"text-gray-400"}`}>{dayInfo.date} {dayInfo.dateName}
+      <div>
+        {todos.map((todo, idx)=>{
+           return todo.date=== locdate ? 
+           <>
+           <li key={idx} onClick={()=>handleItemModalOpen()}>{todo.text}</li>
+            <MonthEditModal todo={todo} dayInfo={dayInfo} itemVisible={itemVisible} handleItemModalClose={handleItemModalClose}></MonthEditModal>
+            </>
+           :null
+        }
+        )}
+      </div>
+      <MonthInputModal dayInfo={dayInfo} visible={visible} handleModalClose={handleModalClose}></MonthInputModal>
+
+    </td>
     </>
   )
 }
