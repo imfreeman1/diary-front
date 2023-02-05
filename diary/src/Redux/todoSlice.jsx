8ea5fba66a14b4805d6db4134e10fc1cf24e2214
eunji1 <ssak1088@gmail.com>
2023-02-05 20:04:39 +0900

    - Feature 캘린더의 일정을 클릭하면 edit모달창이 뜨고 수정, 삭제 기능 넣음

--- diary/src/Redux/todoSlice.jsx
+++ diary/src/Redux/todoSlice.jsx
@@ -1,6 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { v4 } from "uuid"
 
+
 const NAME = 'todoList';
 
 export const todoSlice = createSlice({
@@ -12,7 +13,8 @@ export const todoSlice = createSlice({
       setTodo: (state, action) => {
         const todo = {
           id: v4(),
-          text: action.payload
+          date: action.payload[1]["locdate"],
+          text: action.payload[0]
         }
         state.todos = [...state.todos, todo]
       },
@@ -20,6 +22,12 @@ export const todoSlice = createSlice({
         state.todos = [...state.todos.filter((todo)=>
             todo.id !== action.payload.id
         )]
+      },
+      editTodo: (state, action)=>{
+        const editToText = action.payload[0]
+        const editToTodo = action.payload[1]
+        const findTodo = state.todos.find((todo)=>todo.id===editToTodo.id)
+        findTodo.text = editToText
       },
     },
   });
