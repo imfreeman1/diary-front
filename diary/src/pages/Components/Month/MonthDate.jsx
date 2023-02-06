c51cc83a2f547fcb9b12287ba2fd2ba903b3c085
eunji1 <ssak1088@gmail.com>
2023-02-07 01:17:07 +0900

    - Feature : modal - useOnClickOutSide custom hook 제작: modal의 outside 영역 클릭시 모달창이 닫기게 - MonthDate useOnClickOutSide를 editmodal과 inputmodal에 설정

--- diary/src/pages/Components/Month/MonthDate.jsx
+++ diary/src/pages/Components/Month/MonthDate.jsx
@@ -1,13 +1,16 @@
 import MonthInputModal from '@/pages/Components/MonthModal/MonthInputModal'
+import useOnClickOutside from '@/pages/hooks/useOnClickOutSide'
 import { openModal } from '@/Redux/action'
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import MonthEditModal from '../MonthModal/MonthEditModal'
 
 const MonthDate = ({dayInfo}) => {
   const [visible, setVisible] = useState(false)
+  const inputModalRef = useRef()
 
   const [itemVisible, setItemVisible] = useState(false)
+  const editModalRef = useRef()
 
   const handleModalOpen = () =>{
       setTimeout(() => {
@@ -26,6 +29,9 @@ const MonthDate = ({dayInfo}) => {
     setItemVisible(false)
   }
 
+  useOnClickOutside(inputModalRef, () => setVisible(false))
+  useOnClickOutside(editModalRef, () => setItemVisible(false))
+
   const {locdate} = dayInfo
   const {todos} = useSelector((state)=>state.todoReducer)
   console.log("todos", todos)
@@ -34,17 +40,20 @@ const MonthDate = ({dayInfo}) => {
     <td onDoubleClick={()=>handleModalOpen()} className={`static border w-32 h-32 border-black font-bold text-lg ${dayInfo.isInMonth?dayInfo.isHoliday?"text-[#FF0000]":"text-black":"text-gray-400"}`}>{dayInfo.date} {dayInfo.dateName}
       <div>
         {todos.map((todo, idx)=>{
-           return todo.date=== locdate ? 
-           <>
-           <li key={idx} onClick={()=>handleItemModalOpen()}>{todo.text}</li>
-            <MonthEditModal todo={todo} dayInfo={dayInfo} itemVisible={itemVisible} handleItemModalClose={handleItemModalClose}></MonthEditModal>
-            </>
-           :null
+          return todo.date=== locdate ? 
+          <>
+            <li key={idx} onClick={()=>handleItemModalOpen()}>{todo.text}</li>
+            <div ref={editModalRef}>
+              <MonthEditModal todo={todo} dayInfo={dayInfo} itemVisible={itemVisible} handleItemModalClose={handleItemModalClose}></MonthEditModal>
+            </div>
+          </>
+          :null
         }
         )}
       </div>
+      <div ref={inputModalRef}>
       <MonthInputModal dayInfo={dayInfo} visible={visible} handleModalClose={handleModalClose}></MonthInputModal>
-
+      </div>
     </td>
     </>
   )
