7bf8eccc2ce55c28d2404a0e54a7446cf45ff32a
eunji1 <ssak1088@gmail.com>
2023-02-06 23:39:52 +0900

    임시 저장

--- diary/src/Redux/store.jsx
+++ diary/src/Redux/store.jsx
@@ -2,8 +2,9 @@ import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { googleAuthReducer } from './googleAuthSlice';
 import { calendarReducer } from './calendarSlice';
 import { todoReducer } from './todoSlice';
+import { modalReducer } from './modalSlice';
 
-const rootReducer = combineReducers({googleAuthReducer, calendarReducer, todoReducer})
+const rootReducer = combineReducers({googleAuthReducer, calendarReducer, todoReducer, modalReducer})
 
 export const store = configureStore({
   reducer: rootReducer,
