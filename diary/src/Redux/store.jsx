b8b5d01fdfad5e2f8d3b60b390288bb81de9e171
eunji1 <ssak1088@gmail.com>
2023-02-07 01:36:33 +0900

    필요없어진 파일 정리

--- diary/src/Redux/store.jsx
+++ diary/src/Redux/store.jsx
@@ -2,9 +2,8 @@ import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { googleAuthReducer } from './googleAuthSlice';
 import { calendarReducer } from './calendarSlice';
 import { todoReducer } from './todoSlice';
-import { modalReducer } from './modalSlice';
 
-const rootReducer = combineReducers({googleAuthReducer, calendarReducer, todoReducer, modalReducer})
+const rootReducer = combineReducers({googleAuthReducer, calendarReducer, todoReducer})
 
 export const store = configureStore({
   reducer: rootReducer,
