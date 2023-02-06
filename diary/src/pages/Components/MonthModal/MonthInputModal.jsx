51fa1dd3c2f214aed2a4cb695923d1bc887f5b10
eunji1 <ssak1088@gmail.com>
2023-02-07 01:17:07 +0900

    - Feature : modal - useOnClickOutSide custom hook 제작: modal의 outside 영역 클릭시 모달창이 닫기게 - MonthDate useOnClickOutSide를 editmodal과 inputmodal에 설정

--- diary/src/pages/Components/MonthModal/MonthInputModal.jsx
+++ diary/src/pages/Components/MonthModal/MonthInputModal.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 import { useDispatch } from 'react-redux';
 import Button from '../Button';
 
-const MonthInputModal = ({dayInfo, visible, handleModalClose}) => {
+const MonthInputModal = ({ dayInfo, visible, handleModalClose }) => {
   const dispatch = useDispatch()
   const [text, setText] = useState("")
 
@@ -19,12 +19,13 @@ const MonthInputModal = ({dayInfo, visible, handleModalClose}) => {
 
   return (
     (visible && dayInfo.locdate ?
-      <div className='z-0 absolute inset-x-auto w-96 h-fit bg-white text-right select-none rounded drop-shadow-2xl'>
+      <div className='z-0 absolute inset-x-auto w-96 h-fit bg-white text-right select-none rounded drop-shadow-2xl'
+      >
         <div className='m-3'>
           <Button onClick={()=>handleModalClose()} content="X" />
         </div>
         <div className='text-left m-10'>
-          <form  onSubmit={handleKeyPress}>
+          <form onSubmit={handleKeyPress}>
             <span>■ </span><input className = "text-xl border-b-2 " placeholder='입력하세요' value={text} onChange={handleChange} ></input>
           </form>
         </div>
