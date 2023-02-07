6b60f1d61227348e09aed3762c26bdbf39ce4f9e
eunji1 <ssak1088@gmail.com>
2023-02-07 23:56:16 +0900

    - Feature: todoslice - todo를 배열에 넣어 한꺼번에 관리했지만 날짜별로 객체로 바꾸어 넣어줬다.

--- diary/src/Redux/todoSlice.jsx
+++ diary/src/Redux/todoSlice.jsx
@@ -7,27 +7,30 @@ const NAME = 'todoList';
 export const todoSlice = createSlice({
     name: NAME,
     initialState: {
-      todos: [],
+      todos: {},
     },
     reducers: {
       setTodo: (state, action) => {
+        const date = action.payload.dayInfo["locdate"]
         const todo = {
           id: v4(),
-          date: action.payload[1]["locdate"],
-          text: action.payload[0]
+          date: date,
+          text: action.payload.text
         }
-        state.todos = [...state.todos, todo]
+        if(!state.todos[date]) {
+          state.todos[date]=[todo]
+        } else state.todos[date] = [...state.todos[date], todo]
       },
       delTodo: (state, action) => {
-        state.todos = [...state.todos.filter((todo)=>
+        const date = action.payload.date
+        state.todos[date] = [...state.todos[date].filter((todo)=>
             todo.id !== action.payload.id
         )]
       },
       editTodo: (state, action)=>{
-        const editToText = action.payload[0]
-        const editToTodo = action.payload[1]
-        const findTodo = state.todos.find((todo)=>todo.id===editToTodo.id)
-        findTodo.text = editToText
+        const date = action.payload.todo.date
+        const findTodo = state.todos[date].find((todo)=>todo.id===action.payload.todo.id)
+        findTodo.text = action.payload.text
       },
     },
   });
