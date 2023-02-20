cf2b1c8fef0c26d00d5f5c96f8075d9f15dbda78
eunji1 <ssak1088@gmail.com>
2023-02-21 01:19:06 +0900

    - Feature : Daily 왼쪽 상단에 달력 넣어서 선택한 날짜로 이동할 수 있게 함

--- diary/src/pages/Components/Daily/DailyDisplay.jsx
+++ diary/src/pages/Components/Daily/DailyDisplay.jsx
@@ -2,11 +2,15 @@ import Tiptap from '@/pages/Components/Tiptap/Tiptap'
 import React from 'react'
 import RoundLine from '../RoundLine'
 
-const DailyDisplay = () => {
-    const dateInDaily = new Date();
-    const [year, month, date] = [dateInDaily.getFullYear(), dateInDaily.getMonth()+1, dateInDaily.getDate()]
+const DailyDisplay = ({selectedDate}) => {
+    let dateInDaily = new Date()
+    if(selectedDate) {
+        dateInDaily = selectedDate
+    }
+    let [year, month, date] = [dateInDaily.getFullYear(), dateInDaily.getMonth()+1, dateInDaily.getDate()]
     const WEEKDAY = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
-    const week = WEEKDAY[dateInDaily.getDay()]
+    let week = WEEKDAY[dateInDaily.getDay()]
+
   return (
     <div >
         <div className='h-fit mx-20 my-5 p-2 border-4 border-gray-100 shadow-sm'> 
@@ -23,9 +27,9 @@ const DailyDisplay = () => {
                 <RoundLine/>
                 <RoundLine/>
             </div>
-            <div className='flex text-center text-xl'>
-                <div className='font-bold basis-1/2'>
-                   {year} / {month} / {date} 
+            <div className='flex text-center text-xl font-bold'>
+                <div className='basis-1/2'>
+                   {year} . {month} . {date} 
                 </div>
                 <div className='basis-1/2 text-white bg-slate-400'>
                     {week}
@@ -33,13 +37,10 @@ const DailyDisplay = () => {
             </div>
             <div className='m-10'>
                 <label className='block mb-2 text-md font-medium text-gray-900 dark:text-gray-400'>Title</label>
-                <textarea rows="1" className='block p-2.5 w-full text-lg text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500' placeholder='title...'></textarea>
+                <textarea rows="1" maxLength="10" className='block p-2.5 w-full text-lg text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:outline-none focus:ring-blue-500 focus:border-blue-500' placeholder='title...'></textarea>
             </div>
             <div className='border-2 border-gray-200 overflow-hidden'>
                 <Tiptap></Tiptap>
-                <hr className='bg-black'></hr>
-                <hr className='bg-black'></hr>
-                <hr className='bg-black'></hr>
             </div>
 
 
