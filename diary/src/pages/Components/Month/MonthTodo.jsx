146de4ead909ed3f729777082f8808d1391c2614
eunji1 <ssak1088@gmail.com>
2023-02-21 21:20:00 +0900

    - month todo개수 제한 2-3개

--- diary/src/pages/Components/Month/MonthTodo.jsx
+++ diary/src/pages/Components/Month/MonthTodo.jsx
@@ -8,7 +8,6 @@ import MonthTodoItem from './MonthTodoItem';
 const MonthTodo = ({dayInfo}) => {
   const [listVisible, setListVisible] = useState(false)
   const listModalRef = useRef()
-
   const handleListModalOpen = () => {
     setTimeout(()=> {
       setListVisible(true)
@@ -22,15 +21,15 @@ const MonthTodo = ({dayInfo}) => {
 
   const {locdate} = dayInfo
   const {todos} = useSelector((state)=>state.todoReducer)
-
+  const viewNum = todos[locdate]?(todos[locdate].length>3?2:3):false
   return (
     <>
     {todos[locdate]?
       todos[locdate].map((todo, idx)=>{
-        if(idx<2){
+        if(idx<viewNum){
           return <MonthTodoItem idx={idx} todo={todo} dayInfo={dayInfo} />
         } 
-        if(idx===2){
+        if(idx===viewNum){
           return <>
           <Button onClick={()=>handleListModalOpen()} content="더보기" />
           <div ref = {listModalRef}>
