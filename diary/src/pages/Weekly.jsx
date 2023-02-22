7fbb2c9ff2f01cb53e6dd29b3ca80afd071f21cf
eunji1 <ssak1088@gmail.com>
2023-02-23 02:37:45 +0900

    - week weekredux, lastweek, nextweek 전후 이동가능 버튼 weeks : 몇주차인지 확인

--- diary/src/pages/Weekly.jsx
+++ diary/src/pages/Weekly.jsx
@@ -1,52 +1,42 @@
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 import DatepickerComponent from './Components/DatepickerComponent/DatepickerComponent';
-import WeeklyDisplay from './Components/Weekly/WeeklyDisplay'
 import Button from './Components/Button'
-
-const monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-const days = ["Weekly", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+import WeeklyDisplay from './Components/Weekly/WeeklyDisplay'
+import useGetWeekly from './hooks/useGetWeekly'
+import { setWeek } from '@/Redux/action';
 
 const Weekly = () => {
   const dateInWeekly = new Date()
-  let [year, month, date] = [dateInWeekly.getFullYear(), dateInWeekly.getMonth()+1, dateInWeekly.getDate()]
+  const [selectedDate, setSelectedDate] = useState(dateInWeekly)
+  let [year, month, weeks] = [selectedDate.getFullYear(), selectedDate.getMonth()+1, selectedDate.getDate()/7>>0]
+  const dispatch = useDispatch();
+  const {weeklyPlanner} = useSelector((state)=> state.weeklyReducer);
 
-  const getWeekly = () =>{
-    let week = dateInWeekly.getDay()
-    let calcMon = dateInWeekly.getDate() - week; 
-    let nextDate = (calc) =>{
-      return new Date(dateInWeekly.setDate(calc)).toISOString().substring(0, 10);
-    } 
-    const weeklyPlan = []
-    for(let i=0; i<days.length; i++){
-      let weekObj={}
-      weekObj.days = days[i] 
-      if(i) {
-        weekObj.date = calcMon + i 
-        weekObj.locdate = nextDate(calcMon + i)
-      }
-      weeklyPlan.push(weekObj)
-    }
-    return weeklyPlan
+  useEffect(() => {
+    dispatch(setWeek(useGetWeekly(selectedDate)))
+  }, [selectedDate])
+
+  const lastWeek = () =>{
+    setSelectedDate(new Date(selectedDate.setDate(selectedDate.getDate()-7)))
   }
-
-  const weeklyPlan=getWeekly()
-
+  const nextWeek = () =>{
+    setSelectedDate(new Date(selectedDate.setDate(selectedDate.getDate()+7)))
+  }
   return (
     <div className='h-screen bg-[#9DBC9D] text-center'>Weekly
         <DatepickerComponent selectedDate={selectedDate} setSelectedDate={setSelectedDate}/>
         <div className='w-fit h-fit rounded border-2 bg-white shadow-sm mt-10 mx-auto'>
-            <div className='text-2xl font-bold text-green-900 text-left ml-5'>{year}. {month}</div>
+            <div className='text-2xl font-bold text-green-900 text-left ml-5'>{year}. {month} - {weeks+1}Weeks</div>
             <div className='flex justify-end gap-5 h-10 text-right mr-5'> 
-              <Button content="W1"></Button>
-              <Button content="W2"></Button>
-              <Button content="W3"></Button>
-              <Button content="W4"></Button>
-              <Button content="W5"></Button>
+              <Button content="<" onClick={lastWeek}></Button>
+              <Button content=">" onClick={nextWeek}></Button>
             </div>
             
             <div className='m-3 mx-5 grid grid-cols-4 shadow'>
-                {weeklyPlan.map((day, i)=>
+                {weeklyPlanner.length?weeklyPlanner.map((day, i)=>
                 <WeeklyDisplay key={i} day={day}/>
-                )}
+                ):null}
             </div>
         </div>
     </div>
