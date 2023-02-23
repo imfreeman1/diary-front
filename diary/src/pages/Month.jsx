4112b78d5592a71587be0b1f5df85370effb1478
eunji1 <ssak1088@gmail.com>
2023-02-23 15:36:28 +0900

    month title월 표시 수정, 달 이동 버튼 1-12월까지만 되게

--- diary/src/pages/Month.jsx
+++ diary/src/pages/Month.jsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { setCal } from '@/Redux/action';
 import MonthWeek from './Components/Month/MonthWeek';
 import useCalendar from './hooks/useCalendar';
-import Button from './Components/Button';
+import { BiCaretUp, BiCaretDown } from "react-icons/bi";
 
 const dayOfTheWeek = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
 const MonthList = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
@@ -11,31 +11,34 @@ const MonthList = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "
 const MonthPage = () => {
 
   const date = new Date();
+  const year = date.getFullYear()
   const [month, setMonth] = useState(date.getMonth())
   const dispatch = useDispatch();
   const {calendar} = useSelector((state)=> state.calendarReducer);
 
   const moveToLastMonth = () =>{
-      setMonth(month-1)
+      if(month>0) setMonth(month-1)
     }
   const moveToNextMonth = () =>{
-      setMonth(month+1)
+      if(month<11) setMonth(month+1)
     }
   useEffect(() => {
-    dispatch(setCal(useCalendar("2023",MonthList[month])))
+    dispatch(setCal(useCalendar(year,MonthList[month])))
   }, [month])
 
   return (
     <div className='flex justify-center pt-5 h-screen w-full bg-gray-100'>
       <div className='bg-white m-5 h-fit'>
-        <div className='flex text-4xl w-min px-6 mx-auto'>
-          <Button onClick={()=>moveToLastMonth()} content="<"></Button>
-          <div className='text-4xl border-black border-2 rounded-[50%] w-min px-6 m-3'>{month+1}</div>
-          <Button onClick={()=>moveToNextMonth()} content=">"></Button>
+        <div className='flex'>
+          <p className='text-5xl w-fit px-6 m-3 text-gray-800'>{month+1}월</p>
+          <div className='text-3xl w-min px-6 my-auto'>
+            <BiCaretUp onClick={()=>moveToNextMonth()} className="cursor-pointer hover:text-red-700 hover:ring" />
+            <BiCaretDown onClick={()=>moveToLastMonth()} className="cursor-pointer hover:text-red-700 hover:ring" />
+          </div>
         </div>
-        <div className='flex mb-2 mt-4'>
+        <div className='flex mb-2 mt-4 border-t-2'>
           {dayOfTheWeek.map((val, idx)=> 
-            <div className={`w-36 flex justify-center ${val==="Sun"? "text-[#FF0000]":""}`} key={idx}>{val[0]}</div>
+            <div className={`flex w-36  text-lg font-bold justify-center ${val==="Sun"? "text-[#FF0000]":""}`} key={idx}>{val[0]}</div>
         )}
         </div>
         <table className='border-collapse border border-black'>
