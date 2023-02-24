28deb4022824003d1a1f194a976fccb2aba2be32
imfreeman1 <dkfrhkt@gmail.com>
2023-02-24 17:01:06 +0900

    img crop page 생성
     - img를 sticker화 하기 위한 img crop page
     - react-cropper 사용

---
+++ diary/src/pages/Components/Sticker/EasyCropper.jsx
@@ -0,0 +1,54 @@
+import { useState, useRef } from 'react';
+import Cropper from 'react-cropper';
+import 'cropperjs/dist/cropper.css';
+import { useDispatch } from 'react-redux';
+import Button from '../Button';
+import { addTableSticker } from '@/Redux/stickerAction';
+
+function EasyCropper({ modalHandler }) {
+  const dispatch = useDispatch();
+  const cropperRef = useRef(null);
+  // 유저가 첨부한 이미지
+  const [inputImage, setInputImage] = useState(null);
+  // 유저가 선택한 영역만큼 크롭된 이미지
+
+  // input이 들어왔을때, file을 읽음.
+  const onChange = (e) => {
+    e.preventDefault();
+    let files;
+    if (e.dataTransfer) {
+      files = e.dataTransfer.files;
+    } else if (e.target) {
+      files = e.target.files;
+    }
+    const reader = new FileReader();
+    reader.onload = () => {
+      setInputImage(reader.result);
+    };
+    if (files.length) {
+      reader.readAsDataURL(files[0]);
+    }
+  };
+  const getCropData = () => {
+    const imageElement = cropperRef?.current;
+    const cropper = imageElement?.cropper;
+    if (typeof cropper !== 'undefined') {
+      const stickerURL = cropper.getCroppedCanvas().toDataURL();
+      const [stickerWidth, stickerHeight] = [cropper.cropBoxData.width, cropper.cropBoxData.height];
+      dispatch(addTableSticker({ url: stickerURL, width: stickerWidth, height: stickerHeight }));
+    }
+  };
+  return (
+    <div>
+      <div className="flex justify-end mt-3 mr-3">
+        <Button content="X" onClick={() => modalHandler()} />
+      </div>
+
+      <input type="file" accept="image/*" onChange={onChange} />
+      <Cropper className=" h-[60vh] w-[65vw] mx-12 my-8" src={inputImage} ref={cropperRef} />
+      <Button onClick={getCropData} content="저장" />
+    </div>
+  );
+}
+
+export default EasyCropper;
