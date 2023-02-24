2b40b27a9baa7bee9936e08bd96b2446afddd439
imfreeman1 <dkfrhkt@gmail.com>
2023-02-24 16:57:09 +0900

    uuid를 사용하여 key 설정
     - map함수 사용시 key 값 설정

--- diary/src/pages/Components/NavBar/NavItem.jsx
+++ diary/src/pages/Components/NavBar/NavItem.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import Button from '../Button'
-import { useRouter } from 'next/router'
+import React from 'react';
+import { useRouter } from 'next/router';
+import Button from '../Button';
 
-const hoverBgColorObj = {Month:"hover:bg-orange-200", Weekly:"hover:bg-[#9DBC9D]",};
+const hoverBgColorObj = { Month: 'hover:bg-orange-200', Weekly: 'hover:bg-[#9DBC9D]' };
 
-const NavItem = ({title}) => {
-  const router = useRouter().pathname.replace('/','');
-  
+function NavItem({ title }) {
+  const router = useRouter().pathname.replace('/', '');
   return (
     <li className={`font-bold p-2 w-min ${hoverBgColorObj[router]}`}>
       <Button content={title} />
     </li>
-  )
+  );
 }
 
-export default NavItem
+export default NavItem;
