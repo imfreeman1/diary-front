9182d5f46b0a4ce87cb3354cdf8d8be42de220ad
eunji1 <ssak1088@gmail.com>
2023-03-09 00:58:13 +0900

    dailyslice : dailyContent에 날짜 형식 별로 나타냄

--- diary/src/Redux/dailySlice.jsx
+++ diary/src/Redux/dailySlice.jsx
@@ -14,11 +14,20 @@ export const dailySlice = createSlice({
           state.dailyContent[`D-${locdate}`] = action.payload
         }
       },
+      setDate: (state, action) => {
+        state.dailyContent.date = action.payload
+      },
       setEditor: (state, action) => {
         const locdate = action.payload.locdate
         console.log("Action",action.payload)
         state.dailyContent[`D-${locdate}`].editorContent = action.payload.html
-        // state.date = action.payload.locdate
+      },
+      setTitle: (state, action) => {
+        const locdate = action.payload.date
+        const titleText = action.payload.titleText
+        if(state.dailyContent[`D-${locdate}`]){
+          state.dailyContent[`D-${locdate}`].titleText = titleText
+        }
       },
     }
 })
