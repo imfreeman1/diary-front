6f74a664aa2b18c473e28721f9d4a05614a7afd4
eunji1 <ssak1088@gmail.com>
2023-03-09 00:54:38 +0900

    textarea의 content 날짜 바뀔때 마다 기억하게 redux로 관리

--- diary/src/pages/Components/Daily/DailyDisplay.jsx
+++ diary/src/pages/Components/Daily/DailyDisplay.jsx
@@ -1,9 +1,9 @@
 import Tiptap from '@/pages/Components/Tiptap/Tiptap'
 import useGetDaily from '@/pages/hooks/useGetDaily'
-import { setDaily } from '@/Redux/action'
-import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import RoundLine from '../RoundLine'
+import { setDaily, setTitle } from '@/Redux/action'
+import React, { useEffect, useRef, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import RoundLine from './RoundLine'
 
 /**
  * 
@@ -12,44 +12,49 @@ import RoundLine from '../RoundLine'
  * @returns 
  */
 
-const DailyDisplay = ({selectedDate}) => {
-    const dateInDaily = (selectedDate? selectedDate: new Date())
+const DailyDisplay = () => {
 
     const dispatch = useDispatch()
-    const dailyContent = useGetDaily(dateInDaily)
+
+    const {dailyContent} = useSelector((state)=>state.dailyReducer)
+    const {date} = useSelector((state)=>state.dailyReducer.dailyContent)
+    const daily = useSelector((state)=>state.dailyReducer.dailyContent[`D-${date}`])
+
+    const initContent = daily?daily.titleText?daily.titleText:"":""
+    const [content, setContent] = useState("")
     console.log(dailyContent)
+    
+    useEffect(() => {
+        if(date) dispatch(setDaily(useGetDaily(date)))
+        setContent(initContent)
+    }, [date])
 
     useEffect(() => {
-        dispatch(setDaily(useGetDaily(dateInDaily)))
-    }, [selectedDate])
+        dispatch(setTitle({titleText: content, date: date}))
+    }, [content])
     
+    const handleInput = (e) =>{
+        setContent(e.target.value)
+    }
+
   return (
-    <div>
-        <div className='h-fit mx-20 my-5 p-2 border-4 border-gray-100 shadow-sm'> 
-            <div className='flex m-5'>
-                <RoundLine/>
-                <RoundLine/>
-                <RoundLine/>
-                <RoundLine/>
-                <RoundLine/>
-                <RoundLine/>
-                <RoundLine/>
-                <RoundLine/>
-                <RoundLine/>
-                <RoundLine/>
-                <RoundLine/>
-            </div>
-            <div className='flex text-center text-xl font-bold border'>
-                <div className='basis-1/2'>{dailyContent.locdate}</div>
-                <div className='basis-1/2 text-white bg-slate-400'>{dailyContent.week}</div>
-            </div>
-            <div className='m-5 my-20'>
-                <label className='block mb-2 text-md font-medium text-gray-900 dark:text-gray-400'>Title</label>
-                <textarea rows="1" className='block min-h-[52px] max-h-[52px] p-2.5 w-full text-lg text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:outline-none focus:ring-blue-300 focus:border-blue-300' placeholder='...'></textarea>
-            </div>
-            <div className='border-2 border-gray-200 overflow-hidden'>
-                <Tiptap dateInDaily={dateInDaily}/>
-            </div>
+    <div className='h-fit mx-20 my-5 p-2 border-4 border-gray-100 shadow-sm'> 
+        <RoundLine/>
+        <div className='flex text-center text-xl font-bold border'>
+            <div className='basis-1/2'>{daily?daily.locdate:null}</div>
+            <div className='basis-1/2 text-white bg-slate-400'>{daily?daily.week:null}</div>
+        </div>
+        <div className='m-5 my-20'>
+            <label className='block mb-2 text-md font-medium text-gray-900 dark:text-gray-400'>Title</label>
+            <textarea 
+                rows={1}
+                spellCheck="false"
+                value={content}
+                onInput={handleInput}
+                className='block min-h-[52px] max-h-[52px] p-2.5 w-full text-lg text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:outline-none focus:ring-blue-300 focus:border-blue-300'></textarea>
+        </div>
+        <div className='border-2 border-gray-200 overflow-hidden'>
+            <Tiptap />
         </div>
     </div>
   )
