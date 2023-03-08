d814a4743f96f75dcc0a1802944172574bf8f472
eunji1 <ssak1088@gmail.com>
2023-03-09 00:51:54 +0900

    daily - round 파일 위치 바꾸기

---
+++ diary/src/pages/Components/Daily/RoundLine.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const RoundLine = () => {
+  return (
+    <div className='flex m-5'>
+      <div className='w-20 h-20 border-t-2 border-black rounded-full'></div>
+      <div className='w-20 h-20 border-t-2 border-black rounded-full'></div>
+      <div className='w-20 h-20 border-t-2 border-black rounded-full'></div>
+      <div className='w-20 h-20 border-t-2 border-black rounded-full'></div>
+      <div className='w-20 h-20 border-t-2 border-black rounded-full'></div>
+      <div className='w-20 h-20 border-t-2 border-black rounded-full'></div>
+      <div className='w-20 h-20 border-t-2 border-black rounded-full'></div>
+      <div className='w-20 h-20 border-t-2 border-black rounded-full'></div>
+      <div className='w-20 h-20 border-t-2 border-black rounded-full'></div>
+      <div className='w-20 h-20 border-t-2 border-black rounded-full'></div>
+      <div className='w-20 h-20 border-t-2 border-black rounded-full'></div>
+  </div>
+  )
+}
+
+export default RoundLine
