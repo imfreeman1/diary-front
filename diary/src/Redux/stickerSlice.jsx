3f7deeb955eb30ccd4ef9ec43488b6340bd1f302
imfreeman1 <dkfrhkt@gmail.com>
2023-03-21 15:42:05 +0900

    stickerSlice 리팩토링
     - 변수 변경

--- diary/src/Redux/stickerSlice.jsx
+++ diary/src/Redux/stickerSlice.jsx
@@ -1,21 +1,19 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { v4 } from 'uuid';
+import { createSlice } from "@reduxjs/toolkit";
+import { v4 } from "uuid";
 
-const NAME = 'sticker';
-
-// 삭제reducer 필요.
+const NAME = "sticker";
 
 const makeSticker = (num) => {
   const arr = [];
   for (let i = 1; i <= num; i += 1) {
     const exPicture = {
       id: v4(),
-      url: '/Logo/pen.svg',
-      'data-x': 0,
-      'data-y': 0,
+      imgURL: "/Logo/pen.svg",
+      positionX: 0,
+      positionY: 0,
       width: 70,
       height: 70,
-      selected:false
+      selected: false,
     };
     arr.push(exPicture);
   }
@@ -28,63 +26,79 @@ export const stickerSlice = createSlice({
   name: NAME,
   initialState: {
     stickersArray: {
-      Month: [], Table: [...exStickers], Weekly: [], Daily: [], Test: [],
+      Month: [],
+      Table: [...exStickers],
+      Weekly: [],
+      Daily: [],
+      Test: [],
     },
   },
   reducers: {
-    setSticker: ({stickersArray}, {payload:{id,position,origin}}) => {
-      const data = stickersArray.Table.find((sticker) => sticker.id === id);
-      data.id = v4();
-      data['data-x'] = position.x;
-      data['data-y'] = position.y;
+    setSticker: ({ stickersArray }, { payload: { id, position, origin } }) => {
+      const selectedSticker = stickersArray.Table.find(
+        (sticker) => sticker.id === id
+      );
+      selectedSticker.id = v4();
+      selectedSticker.positionX = position.x;
+      selectedSticker.positionY = position.y;
       // if의 있는 조건 결과물을 변수로 한번 빼내자
-      if (stickersArray[origin].filter((sticker)=> sticker.selected===true)) {
-        stickersArray[origin].map((sticker)=> sticker.selected=false);
-      }
-      stickersArray[origin] = [...stickersArray[origin], data];
+      const selectedChecker = stickersArray[origin].some(
+        (sticker) => sticker.selected === true
+      );
+      if (selectedChecker)
+        stickersArray[origin].map((sticker) => (sticker.selected = false));
+      stickersArray[origin] = [...stickersArray[origin], selectedSticker];
     },
-    removeSticker: ({stickersArray}, {payload:{id,origin}}) => {
-      stickersArray[origin]= stickersArray[origin].filter((sticker) => sticker.id !== id);
+    removeSticker: ({ stickersArray }, { payload: { id, origin } }) => {
+      stickersArray[origin] = stickersArray[origin].filter(
+        (sticker) => sticker.id !== id
+      );
     },
-    setInit: ({stickersArray}, {payload:{origin,data}}) => {
-      stickersArray[origin] = [...data];
+    setInit: ({ stickersArray }, { payload: { origin, selectedSticker } }) => {
+      stickersArray[origin] = [...selectedSticker];
     },
-    setPosition: ({stickersArray}, {payload:{origin,id,position}}) => {
-      const data = stickersArray[origin].find((sticker) => sticker.id === id);
-      if (!data.selected) stickersArray[origin].map((sticker)=> sticker.selected=false);
-      data['data-x'] = position.x;
-      data['data-y'] = position.y;
+    setPosition: ({ stickersArray }, { payload: { origin, id, position } }) => {
+      const selectedSticker = stickersArray[origin].find(
+        (sticker) => sticker.id === id
+      );
+      if (!selectedSticker.selected)
+        stickersArray[origin].map((sticker) => (sticker.selected = false));
+      selectedSticker.positionX = position.positionX;
+      selectedSticker.positionY = position.positionY;
     },
-    setResize: ({stickersArray}, {payload:{origin,id,size}}) => {
-      const data = stickersArray[origin].find((sticker) => sticker.id === id);
-      data.height = size.height;
-      data.width = size.width;
+    setResize: ({ stickersArray }, { payload: { origin, id, size } }) => {
+      const selectedSticker = stickersArray[origin].find(
+        (sticker) => sticker.id === id
+      );
+      selectedSticker.height = size.height;
+      selectedSticker.width = size.width;
     },
-    addTableSticker: ({stickersArray}, {payload:{url,size}}) => {
+    addTableSticker: ({ stickersArray }, { payload: { imgURL, size } }) => {
       const newSticker = {
         id: v4(),
-        url: url,
-        'data-x': 0,
-        'data-y': 0,
+        imgURL,
+        positionX: 0,
+        positionY: 0,
         width: size.width,
         height: size.height,
-        selected:false
+        selected: false,
       };
       stickersArray.Table.push(newSticker);
     },
-    setSelect: ({stickersArray}, {payload:{origin,id}}) => {
-      // 결과물 한번 뺴놓자
-      if (stickersArray[origin].some((sticker)=> sticker.selected===true)) {
-        stickersArray[origin].map((sticker)=> sticker.selected=false);
-      }
-      // 최악의 변수명 data
-      const data = stickersArray[origin].find((sticker) => sticker.id === id);
-      data.selected = !(data.selected);
+    setSelect: ({ stickersArray }, { payload: { origin, id } }) => {
+      const selectedChecker = stickersArray[origin].some(
+        (sticker) => sticker.selected === true
+      );
+      if (selectedChecker)
+        stickersArray[origin].map((sticker) => (sticker.selected = false));
+      const selectedSticker = stickersArray[origin].find(
+        (sticker) => sticker.id === id
+      );
+      selectedSticker.selected = !selectedSticker.selected;
     },
-    resetSelect: ({stickersArray}, {payload:{origin}}) => {
-      stickersArray[origin].map((sticker)=> sticker.selected=false);
-    }
-
+    resetSelect: ({ stickersArray }, { payload: { origin } }) => {
+      stickersArray[origin].map((sticker) => (sticker.selected = false));
+    },
   },
 });
 
