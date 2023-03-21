e821c5a29904377e37e27001f5b12dda1f8686b9
imfreeman1 <dkfrhkt@gmail.com>
2023-03-21 15:40:42 +0900

    StickerMakerModal 컴포넌트 수정
     - modal 페이지 변경

--- diary/src/pages/Components/SideBar/SideBarPresent/StickerMakeModal.jsx
+++ diary/src/pages/Components/SideBar/SideBarPresent/StickerMakeModal.jsx
@@ -1,24 +1,21 @@
-import React, { useEffect, useRef } from 'react';
-import EasyCropper from '../Sticker/EasyCropper';
-
+import React, { useEffect, useRef } from "react";
+import EasyCropper from "../../Sticker/EasyCropper";
 
 function StickerMakeModal({ modalHandler }) {
   const focusRef = useRef(null);
 
   useEffect(() => {
     focusRef.current.focus();
-  }, [])
-  
+  }, []);
 
   return (
-    <div className=" h-screen w-screen" >
-      <div className="h-screen bg-opacity-75 bg-gray-400 flex justify-center items-center" onClick={(e)=> e.stopPropagation()}>
+    <div className=" h-screen w-screen">
+      <div className="h-screen bg-opacity-75 bg-gray-400 flex justify-center items-center">
         <div className=" fixed border-2 bg-white opacity-100" ref={focusRef}>
           <EasyCropper modalHandler={modalHandler} />
         </div>
       </div>
     </div>
-
   );
 }
 
