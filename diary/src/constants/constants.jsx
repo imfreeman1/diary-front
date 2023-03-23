e48f4456cf70a81c85185227da6695f84262abe5
imfreeman1 <dkfrhkt@gmail.com>
2023-03-23 13:59:25 +0900

    navBar와 공용 상수 파일 분할
     - navBar에서만 사용되는 상수 navbarConstants파일로 분할 함.

--- diary/src/constants/constants.jsx
+++ diary/src/constants/constants.jsx
@@ -10,29 +10,7 @@ import { useRouter } from "next/router";
 export const SIDEBAR_OPENER = "열림";
 export const SIDEBAR_CLOSER = "닫힘";
 export const STICKER_MAKER_BUTTON_CONTENT = "스티커 만들기";
-export const NAV_ITEM_LIST = [
-  "Cover",
-  "2023",
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "11",
-  "12",
-  "Weekly",
-  "Daily",
-  "Personal",
-];
-export const NAVBAR_HOVER_BG_COLOR_OBJECT = {
-  Month: "hover:bg-gray-100",
-  Weekly: "hover:bg-[#9DBC9D]",
-};
+
 export const LANDING_PAGE_CONTENT = "Let's Write";
 export const STICKER_CLOSE_BUTTON_CONTENT = "X";
 export const STICKER_SAVE_BUTTON_CONTENT = "저장";
@@ -42,13 +20,11 @@ export const SELECT_IN_STICKER_DIV = "div";
 export const STICKER_SELECTOR_ID = (id) => `[id="${id}"]`;
 export const CURRENT_ROUTER_PATH = () => useRouter().pathname.replace("/", "");
 
-export const STICKER_IMG_SIZE_OBJECT = (width, height, x, y) => {
-  return {
-    width: `${width}px`,
-    height: `${height}px`,
-    transform: `translate(${x}px, ${y}px)`,
-  };
-};
+export const STICKER_IMG_SIZE_OBJECT = (width, height, x, y) => ({
+  width: `${width}px`,
+  height: `${height}px`,
+  transform: `translate(${x}px, ${y}px)`,
+});
 
 export const STICKER_POSITION_TRANSLATOR = (position) =>
   `translate(${position.positionX}px, ${position.positionY}px)`;
