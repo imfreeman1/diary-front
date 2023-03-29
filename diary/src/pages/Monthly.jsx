d12976198f10f5aeba61944f9d6e6327c48fc53f
imfreeman1 <dkfrhkt@gmail.com>
2023-03-29 22:04:54 +0900

    Monthly page 수정
     -Monthly component 에 Sticker, SideBar, NavBar component 추가

--- diary/src/pages/Monthly.jsx
+++ diary/src/pages/Monthly.jsx
@@ -1,15 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { BiCaretUp, BiCaretDown } from 'react-icons/bi';
-import { v4 } from 'uuid';
-import { setCal } from '@/Redux/action';
-import MonthWeekPresenter from './Components/Month/MonthWeek/MonthWeekPresenter';
-import useMonthCalendar from './Utils/useMonthCalendar';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { BiCaretUp, BiCaretDown } from "react-icons/bi";
+import { v4 } from "uuid";
+import { setCal } from "@/Redux/action";
+import MonthWeekPresenter from "./Components/Month/MonthWeek/MonthWeekPresenter";
+import useMonthCalendar from "./Utils/useMonthCalendar";
 import {
   DAY_OF_WEEK,
   MONTH_LIST,
   MONTH_INDICATING,
-} from '../Constants/monthlyConstants';
+} from "../Constants/monthlyConstants";
+import NavBarContainer from "./Components/NavBar/NavBarContainer";
+import StickerContainer from "./Components/Sticker/StickerContainer";
+import SideBarContainer from "./Components/SideBar/SideBarContainer";
+import { CURRENT_ROUTER_PATH } from "@/Constants/constants";
 
 /**
  * @param {selectedMonth} number, 기본 날짜는 현재 월 (0-11)
@@ -21,6 +25,10 @@ function MonthlyPage() {
   const [yearInMonth, setYearInMonth] = useState(dateInMonth.getFullYear());
   const [selectedMonth, setSelectedMonth] = useState(dateInMonth.getMonth());
 
+  const stickerList = useSelector(
+    (state) => state.stickerReducer.stickersArray
+  );
+  const currRouter = CURRENT_ROUTER_PATH();
   const dispatch = useDispatch();
   const { monthCalendar } = useSelector((state) => state.monthCalendarReducer);
 
@@ -44,46 +52,64 @@ function MonthlyPage() {
   };
 
   return (
-    <div className="flex justify-center p-10 h-full w-full bg-gray-100">
-      <div className="bg-zinc-50 border p-2 my-10 h-fit shadow-lg rounded">
-        <div className="flex gap-5">
-          <div className="text-3xl w-min px-6 my-auto">
-            <BiCaretUp
-              onClick={() => moveToNextMonth()}
-              className="cursor-pointer text-gray-700 hover:text-red-700 hover:ring hover:ring-gray-300"
-            />
-            <BiCaretDown
-              onClick={() => moveToLastMonth()}
-              className="cursor-pointer text-gray-700 hover:text-red-700 hover:ring hover:ring-gray-300"
-            />
-          </div>
-          <p className="text-5xl w-fit px-6 m-3 text-gray-700 select-none">
-            {selectedMonth + 1}
-            {MONTH_INDICATING}
-          </p>
-          <p className="text-2xl text-green-900 select-none">{yearInMonth}</p>
-        </div>
-        <div className="flex my-2 border-2">
-          {DAY_OF_WEEK.map((day) => (
-            <div
-              className={`flex border w-36 text-lg font-bold justify-center bg-gray-200 ${
-                day === 'Sun' ? 'text-[#FF0000]' : ''
-              }`}
-              key={v4()}
-            >
-              {day}
+    <>
+      <NavBarContainer />
+    <div classname="flex justify-center p-10 h-full w-full bg-gray-100">
+      <div classname="bg-zinc-50 border p-2 my-10 h-fit shadow-lg rounded">
+        <div classname="flex gap-5">
+          <div classname="text-3xl w-min px-6 my-auto">
+              <BiCaretUp
+                onClick={() => moveToNextMonth()}
+                className="cursor-pointer text-gray-700 hover:text-red-700 hover:ring hover:ring-gray-300"
+              />
+              <BiCaretDown
+                onClick={() => moveToLastMonth()}
+                className="cursor-pointer text-gray-700 hover:text-red-700 hover:ring hover:ring-gray-300"
+              />
             </div>
-          ))}
+            <p className="text-5xl w-fit px-6 m-3 text-gray-700 select-none">
+              {selectedMonth + 1}
+              {MONTH_INDICATING}
+            </p>
+            <p className="text-2xl text-green-900 select-none">{yearInMonth}</p>
+          </div>
+          <div className="flex my-2 border-2">
+            {DAY_OF_WEEK.map((day) => (
+              <div
+                className={`flex border w-36 text-lg font-bold justify-center ${
+                  day === "Sun" ? "text-[#FF0000]" : ""
+                }`}
+                key={v4()}
+              >
+                {day[0]}
+              </div>
+            ))}
+          </div>
+          <table className="border-collapse border border-gray-400">
+            {monthCalendar.length
+              ? monthCalendar.map((week) => (
+                  <MonthWeekPresenter key={v4()} week={week} />
+                ))
+              : null}
+          </table>
         </div>
-        <table className="border-collapse border border-gray-400">
-          {monthCalendar.length
-            ? monthCalendar.map((week) => (
-              <MonthWeekPresenter key={v4()} week={week} />
-            ))
-            : null}
-        </table>
+        {stickerList[currRouter]?.map((sticker) => (
+          <StickerContainer
+            imgURL={sticker.imgURL}
+            key={v4()}
+            id={sticker.id}
+            position={{
+              positionX: sticker.positionX,
+              positionY: sticker.positionY,
+            }}
+            width={sticker.width}
+            height={sticker.height}
+            selected={sticker.selected}
+          />
+        ))}
+        <SideBarContainer />
       </div>
-    </div>
+    </>
   );
 }
 
