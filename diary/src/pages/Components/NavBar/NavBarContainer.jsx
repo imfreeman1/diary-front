6b15338a063a073ee6254137497f6a36becda8fe
imfreeman1 <dkfrhkt@gmail.com>
2023-03-30 18:11:39 +0900

    NavBar 수정
     - router 기능 구현

--- diary/src/pages/Components/NavBar/NavBarContainer.jsx
+++ diary/src/pages/Components/NavBar/NavBarContainer.jsx
@@ -1,6 +1,30 @@
+import { useRouter } from "next/router";
 import React from "react";
 import NavBarPresent from "./NavBarPresent";
 
-const NavBarContainer = () => <NavBarPresent />;
+const NavBarContainer = ({ setSelectedMonth }) => {
+  const router = useRouter();
+  const currMonth = new Date().getMonth();
+  const routerSelector = (e) => {
+    const routerKey = e.target.innerText;
+    switch (routerKey) {
+      case "Cover":
+        return router.push("/Main");
+      case "Weekly":
+        return router.push(routerKey);
+      case "Daily":
+        return router.push(routerKey);
+      case "Personal":
+        return;
+      case "2023":
+        setSelectedMonth(currMonth);
+        return router.push("/Monthly");
+      default:
+        setSelectedMonth(+routerKey - 1);
+        return router.push("/Monthly");
+    }
+  };
+  return <NavBarPresent routerSelector={routerSelector} />;
+};
 
 export default NavBarContainer;
