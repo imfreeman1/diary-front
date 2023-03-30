8a6e0466d66380434192fdddfc842e9c475af444
imfreeman1 <dkfrhkt@gmail.com>
2023-03-30 18:10:17 +0900

    Monthly 페이지 수정
     - setSelectedMonth 함수 props drilling

--- diary/src/pages/Monthly.jsx
+++ diary/src/pages/Monthly.jsx
@@ -53,7 +53,7 @@ function MonthlyPage() {
 
   return (
     <>
-      <NavBarContainer />
+      <NavBarContainer setSelectedMonth={setSelectedMonth} />
       {stickerList[currRouter]?.map((sticker) => (
         <StickerContainer
           imgURL={sticker.imgURL}
@@ -68,10 +68,10 @@ function MonthlyPage() {
           selected={sticker.selected}
         />
       ))}
-    <div className="flex justify-center p-10 h-full w-full bg-gray-100">
-      <div className="bg-zinc-50 border p-2 my-10 h-fit shadow-lg rounded">
-        <div className="flex gap-5">
-          <div className="text-3xl w-min px-6 my-auto">
+      <div className="flex justify-center p-10 h-full w-full bg-gray-100">
+        <div className="bg-zinc-50 border p-2 my-10 h-fit shadow-lg rounded">
+          <div className="flex gap-5">
+            <div className="text-3xl w-min px-6 my-auto">
               <BiCaretUp
                 onClick={() => moveToNextMonth()}
                 className="cursor-pointer text-gray-700 hover:text-red-700 hover:ring hover:ring-gray-300"
