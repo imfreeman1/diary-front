3aa38dff3480b6aba51415e1e63e3aafd5714964
imfreeman1 <dkfrhkt@gmail.com>
2023-04-01 17:56:38 +0900

    필요 없는 주석 제거.

--- diary/src/Redux/monthSelectorSlice.jsx
+++ diary/src/Redux/monthSelectorSlice.jsx
@@ -3,7 +3,6 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const dateInMonth = new Date();
 
-//제가 리덕스로 옮기면서 지금 문제가 생겼는데
 export const monthSelectorSlice = createSlice({
   name: MONTH_SELECTOR_NAME,
   initialState: {
