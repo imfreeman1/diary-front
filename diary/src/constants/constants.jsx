cb9438c14c00bbf91b967f55336083666d58f674
imfreeman1 <dkfrhkt@gmail.com>
2023-04-02 17:42:59 +0900

    constants 파일 수정
     - 사용하지 않는 상수 제거

--- diary/src/constants/constants.jsx
+++ diary/src/constants/constants.jsx
@@ -7,13 +7,7 @@ import { useRouter } from "next/router";
 2. 기능에 역할을 하고 있는가?
 */
 
-export const SIDEBAR_OPENER = "열림";
-export const SIDEBAR_CLOSER = "닫힘";
-export const STICKER_MAKER_BUTTON_CONTENT = "스티커 만들기";
-
 export const LANDING_PAGE_CONTENT = "Let's Write";
-export const STICKER_CLOSE_BUTTON_CONTENT = "X";
-export const STICKER_SAVE_BUTTON_CONTENT = "저장";
 export const SELECT_IN_STICKER_DIV = "div";
 
 //얘들은 hook으로 빼내는게 좋지 않을까? 특히 PATH나 OBJECT 같은 경우.
