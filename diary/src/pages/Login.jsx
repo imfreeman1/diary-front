9842a24c03549ab9cf24b487a965adce029da758
imfreeman1 <dkfrhkt@gmail.com>
2023-04-10 18:50:03 +0900

    Login 페이지 수정
     - 통신에서 image와 image_type추가

--- diary/src/pages/Login.jsx
+++ diary/src/pages/Login.jsx
@@ -16,7 +16,9 @@ const Login = () => {
           let payload = {
             email: resData.email,
             password: resData.password,
-            name: "jih",
+            name: resData.name,
+            image: "",
+            image_type: "",
           };
           await axios.post("/users/signup/", payload);
         } catch (e) {
@@ -37,6 +39,13 @@ const Login = () => {
         type="password"
         placeholder="****************"
         {...register("password")}
+      />
+      <label htmlFor="name">이름</label>
+      <input
+        id="name"
+        type="string"
+        placeholder="이름입력"
+        {...register("name")}
       />
       <button type="submit" disabled={isSubmitting}>
         로그인
