1d2a376c8fb5e7e29d4c8627918dc94a0b4eb3dd
성은지 <96868722+eunji1@users.noreply.github.com>
2023-04-20 19:56:43 +0900

    daily 페이지 수정

--- diary/src/pages/Components/DatepickerComponent/DatepickerComponent.jsx
+++ diary/src/pages/Components/DatepickerComponent/DatepickerComponent.jsx
@@ -3,6 +3,7 @@ import React, { forwardRef, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { useDispatch } from "react-redux";
+import { BiCalendar } from "react-icons/bi";
 /**
  *
  * @param {selectedDate} date
@@ -25,11 +26,12 @@ const DatepickerComponent = ({
   };
   const ExampleCustomInput = forwardRef(({ value, onClick }, ref) => (
     <button
-      className="bg-gray-200 border-2 shadow-md p-2 px-5 rounded"
+      className="flex bg-translate border-double border-4 border-black m-1 p-2 px-5 rounded shadow-inner"
       onClick={onClick}
       ref={ref}
     >
-      {value}
+      <p className="">{value}</p>
+      <BiCalendar size="20" />
     </button>
   ));
   return (
