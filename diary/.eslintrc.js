50f99806ed063e07c15269ad43caa507246b2b91
imfreeman1 <dkfrhkt@gmail.com>
2023-04-21 18:53:32 +0900

    eslint 수정
     - eslint가 작동하지 않는 듯 하여 수정하였음.

---
+++ diary/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb', 'airbnb/hooks', 'next/babel', 'next/core-web-vitals'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'react/function-component-definition': [
+      2,
+      { namedComponents: ['arrow-function', 'function-declaration'] },
+    ],
+    'linebreak-style': 0,
+    'no-extraneous-dependencies': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'jsx-props-no-spreading': 'off',
+    'react/jsx-filename-extension': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'import/no-unresolved': 0,
+    'import/extensions': ['off'],
+    'no-console': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'FunctionExpression',
+      'WithStatement',
+      "BinaryExpression[operator='in']",
+    ],
+  },
+};
