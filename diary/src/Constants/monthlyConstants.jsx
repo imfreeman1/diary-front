15ee8051a7131d496c0a9be8d4904559380074a6
kimud6003 <kimud6003@gmail.com>
2023-05-03 22:34:43 +0900

    - HotFIx Constatns 이름 수정

---
+++ diary/src/Constants/monthlyConstants.jsx
@@ -0,0 +1,28 @@
+export const DAY_OF_WEEK = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+export const MONTH_LIST = [
+  '01',
+  '02',
+  '03',
+  '04',
+  '05',
+  '06',
+  '07',
+  '08',
+  '09',
+  '10',
+  '11',
+  '12',
+];
+
+export const MONTH_DAYS = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+export const MONTH_INDICATING = '월';
+
+export const BASIC_SHOW_TODO = 2;
+
+export const MAX_SHOW_TODO = 3;
+
+export const SHOW_MORE_TODO = (todos) => `일정 ${todos.length - BASIC_SHOW_TODO}개 더보기`;
+
+export const INPUT_PLACEHOLDER = '입력하세요';
---
+++ diary/src/Constants/weeklyConstant.jsx
@@ -0,0 +1,14 @@
+export const DAYS_WEEKLY = [
+  'Weekly',
+  'Mon',
+  'Tue',
+  'Wed',
+  'Thu',
+  'Fri',
+  'Sat',
+  'Sun',
+];
+
+export const WEEKLY_LOGO = 'Weekly';
+
+export const WEEK = 'W-';
