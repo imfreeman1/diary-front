609bd9af3a9358963cbfcc452b4c4247c5285943
imfreeman1 <dkfrhkt@gmail.com>
2023-05-05 17:21:32 +0900

    Landing Page 수정
     - Login 버튼 생성
     - 버튼 클릭 시 Login router로 이동

--- diary/src/pages/Landing.jsx
+++ diary/src/pages/Landing.jsx
@@ -5,9 +5,15 @@ import Logo from 'public/Logo/logo.svg';
 import Pen from 'public/Logo/pen.svg';
 import Lottie from 'lottie-react';
 // import GleLoginContainer from '../Components/GleLoginContainer';
+import Button from 'src/Components/Button';
+import { useRouter } from 'next/router';
 import { LANDING_PAGE_CONTENT } from '../Constants/constants';
 
 function Landing() {
+  const router = useRouter();
+  const onClick = () => {
+    router.push('/Login');
+  };
   return (
     <>
       <div className="flex items-center justify-between px-8 absolute w-full h-12 bg-white">
@@ -25,6 +31,7 @@ function Landing() {
       <div className=" w-full h-screen bg-slate-200 flex flex-col gap-6 items-center justify-center">
         <Lottie animationData={bookDrop} />
         <div className="text-5xl font-bold">{LANDING_PAGE_CONTENT}</div>
+        <Button content="로그인" className="bg-blue-400 w-40 h-11 rounded-xl shadow-lg text-white mt-3" onClick={onClick} />
         {/* <GleLoginContainer /> */}
       </div>
     </>
