517d9807d3220d0dccb75649da383bb56803b1b4
imfreeman1 <dkfrhkt@gmail.com>
2023-05-05 17:24:18 +0900

    navbarItemList 변경
     - itemList에서 년도 버튼이 동적으로 변하면서 constant가 아닌 utils로 변경

---
+++ diary/src/Utils/makeNavItem.jsx
@@ -0,0 +1,23 @@
+const MakeNavItemList = (yearInMonth) => {
+  const NAV_ITEM_LIST = [
+    'Cover',
+    `${yearInMonth}`,
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10',
+    '11',
+    '12',
+    'Weekly',
+    'Daily',
+    'Personal'];
+  return NAV_ITEM_LIST;
+};
+
+export default MakeNavItemList;
