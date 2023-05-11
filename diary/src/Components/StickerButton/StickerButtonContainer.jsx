31c1ad147ef2b39319f765eed3b04a9c3aced14d
imfreeman1 <dkfrhkt@gmail.com>
2023-05-11 19:40:56 +0900

    StickerButtonContainer
     - sticker/write 통신 제작
     - 내부 함수들 Util로 분리

--- diary/src/Components/StickerButton/StickerButtonContainer.jsx
+++ diary/src/Components/StickerButton/StickerButtonContainer.jsx
@@ -3,42 +3,36 @@ import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import { setSticker } from 'src/Redux/action';
 import { CURRENT_ROUTER_PATH } from 'src/Constants/constants';
-import axios from 'src/Utils/api';
+import urlToFile from 'src/Utils/urlToFile';
+import makeFormData from 'src/Utils/makeFormData';
+import utilAxios from 'src/Utils/utilAxios';
 import StickerButtonPresent from './StickerButtonPresent';
 
 function StickerButtonContainer({ sticker }) {
   const dispatch = useDispatch();
   const currRouter = CURRENT_ROUTER_PATH();
-  const makeStickerHandler = (e) => {
-    const stickerPosition = {
-      positionX: e.view.innerWidth / 2,
-      positionY: e.view.innerHeight / 4,
+  const makeStickerHandler = async (e) => {
+    const positionX = e.view.innerWidth / 2;
+    const positionY = e.view.innerHeight / 4;
+    const imgFile = await urlToFile(sticker.imgURL, 'image');
+    const stickerFormDataObject = {
+      id: sticker.id,
+      page_date: '2023-05-10',
+      page_type: currRouter.toLowerCase(),
+      image_name: '이름',
+      image: imgFile,
+      'size[0]': sticker.height,
+      'size[1]': sticker.width,
+      'position[0]': positionX,
+      'position[1]': positionY,
     };
-    const formData = new FormData();
-    formData.append('id', sticker.id);
-    formData.append('size', [sticker.height, sticker.width]);
-    formData.append('page_type', currRouter);
-    formData.append('position', stickerPosition);
-    formData.append('page_date', '2023-05-10');
-    formData.append('image_name', '이름');
-
-    // URL string, method string, router string, pageDate string, stickerData object
-    const test = async (URL, method, router, pageDate, stickerData) => {
-      try {
-        const checker = await axios.request({
-          URL,
-          method,
-          page_type: router,
-          page_date: pageDate,
-          stickerData,
-        });
-        console.log(checker);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    test();
-    dispatch(setSticker({ origin: currRouter, id: sticker.id, position: stickerPosition }));
+    const formData = makeFormData(stickerFormDataObject);
+    await utilAxios('/sticker/write', 'post', formData, {
+      'Content-Type': 'multipart/form-data',
+    });
+    dispatch(setSticker(
+      { origin: currRouter, id: sticker.id, position: { positionX, positionY } },
+    ));
   };
   return (
     <StickerButtonPresent
