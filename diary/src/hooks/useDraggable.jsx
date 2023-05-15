4f8291a8a440902bdd47bfb2909ce711f10bf88f
imfreeman1 <dkfrhkt@gmail.com>
2023-05-15 19:00:44 +0900

    useDraggable 수정
     - useEffect에서 useLayoutEffect로 변경
     - debounce를 util로 빼내기 위한 작업 진행중.(미완)

--- diary/src/hooks/useDraggable.jsx
+++ diary/src/hooks/useDraggable.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useLayoutEffect, useRef } from 'react';
 import interact from 'interactjs';
 import { useDispatch, useSelector } from 'react-redux';
 import { setPosition } from 'src/Redux/action';
@@ -7,59 +7,67 @@ import {
   STICKER_POSITION_TRANSLATOR,
 } from 'src/Constants/constants';
 
+let timer;
+const debounce = (time, callBack) => {
+  clearTimeout(timer);
+  timer = setTimeout(() => {
+    callBack();
+  }, time);
+};
 const useDraggable = (position) => {
   const positions = useRef(null);
-  const stickerTimer = useRef(null);
   const dispatch = useDispatch();
   const currRouter = CURRENT_ROUTER_PATH();
   positions.current = position;
   const currStickersList = useSelector(
     (state) => state.stickerReducer.stickersArray[currRouter],
   );
-  const debounce = (id, time, timer) => {
-    if (timer.current) clearTimeout(timer.current);
-
-    timer.current = setTimeout(() => {
-      dispatch(
-        setPosition({ origin: currRouter, id, position: positions.current }),
-      );
-      timer.current = null;
-    }, time);
-  };
   // 라이브러리 세팅은 config 따로 관리하자
-  useEffect(() => {
-    interact('.draggable').draggable({
-      overlap: 'pointer',
-      restrict: {
-        elementRect: {
-          top: 0,
-          left: 0,
-          bottom: 1,
-          right: 1,
+  useLayoutEffect(
+    () => {
+      interact('.draggable').draggable({
+        overlap: 'pointer',
+        restrict: {
+          elementRect: {
+            top: 0,
+            left: 0,
+            bottom: 1,
+            right: 1,
+          },
         },
-      },
-      listeners: {
-        start: (event) => {
-          const currSticker = currStickersList.find(
-            (sticker) => sticker.id === event.target.id,
-          );
-          positions.current = {
-            positionX: currSticker.positionX,
-            positionY: currSticker.positionY,
-          };
+        listeners: {
+          start: (event) => {
+            const currSticker = currStickersList.find(
+              (sticker) => sticker.id === event.target.id,
+            );
+            positions.current = {
+              positionX: currSticker.positionX,
+              positionY: currSticker.positionY,
+            };
+          },
+          move: (event) => {
+            const styleOfTarget = event.target.style;
+            positions.current.positionX += event.dx;
+            positions.current.positionY += event.dy;
+            styleOfTarget.transform = STICKER_POSITION_TRANSLATOR(
+              positions.current,
+            );
+            const callBackDispatch = () => dispatch(
+              setPosition({
+                origin: currRouter,
+                id: event.target.id,
+                position: positions.current,
+              }),
+            );
+            debounce(2000, callBackDispatch);
+          },
         },
-        move: (event) => {
-          const styleOfTarget = event.target.style;
-          positions.current.positionX += event.dx;
-          positions.current.positionY += event.dy;
-          styleOfTarget.transform = STICKER_POSITION_TRANSLATOR(
-            positions.current,
-          );
-          debounce(event.target.id, 300, stickerTimer);
-        },
-      },
-    });
-  });
+      });
+    },
+    // return (()=> {
+    // })
+    [],
+  );
 };
 
 export default useDraggable;
