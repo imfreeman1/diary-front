cb600d71fe9d83d7d7c6aaba33d8e70740835333
kimud6003 <kimud6003@gmail.com>
2023-05-18 23:01:19 +0900

    - Feature Weekly 메뉴바 설정
    - Daily와 달리 highjlight, textalign 기능 추가됨

---
+++ diary/src/Components/WeeklyTipTap/WeeklyEditorMenuBar.jsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import {
+  BiBold, BiAlignLeft, BiAlignMiddle, BiAlignRight, BiHighlight, BiStrikethrough,
+} from 'react-icons/bi';
+import PropTypes from 'prop-types';
+import { Editor } from '@tiptap/react';
+import WeeklyEditorMenuBarButton from './WeeklyEditorMenuBarButton';
+
+const WeeklyEditorMenuBar = ({ editor }) => {
+  if (!editor) {
+    return null;
+  }
+  // const editorFocus = () => {
+  //   console.log(editor.chain().focus());
+  //   return editor.chain().focus();
+  // };
+  // const disabledEditorFocus = () => {
+  //   console.log(!editor.can().chain().focus());
+  //   return !editor.can().chain().focus();
+  // };
+
+  return (
+    <div className="flex flex-row flex-1">
+      <WeeklyEditorMenuBarButton
+        onClick={() => editor.chain().focus().toggleBold().run()}
+        disabled={
+          !editor.can().chain().focus()
+            .toggleBold()
+            .run()
+        }
+        className={editor.isActive('bold') ? 'is-active' : ''}
+        content={<BiBold size="28" />}
+      />
+      <WeeklyEditorMenuBarButton
+        onClick={() => editor.chain().focus().toggleHighlight().run()}
+        disabled={
+          !editor.can().chain().focus()
+            .toggleItalic()
+            .run()
+        }
+        className={editor.isActive('highlight') ? 'is-active' : ''}
+        content={<BiHighlight size="28" />}
+      />
+      <WeeklyEditorMenuBarButton
+        onClick={() => editor.chain().focus().toggleStrike().run()}
+        disabled={
+          !editor.can().chain().focus()
+            .toggleStrike()
+            .run()
+        }
+        className={editor.isActive('strike') ? 'is-active' : ''}
+        content={<BiStrikethrough size="28" />}
+      />
+      <WeeklyEditorMenuBarButton
+        onClick={() => editor.chain().focus().setTextAlign('left').run()}
+        disabled={
+          !editor.can().chain().focus()
+            .toggleStrike()
+            .run()
+        }
+        className={editor.isActive({ textAlign: 'left' }) ? 'is-active' : ''}
+        content={<BiAlignLeft size="28" />}
+      />
+
+      <WeeklyEditorMenuBarButton
+        onClick={() => editor.chain().focus().setTextAlign('center').run()}
+        disabled={
+          !editor.can().chain().focus()
+            .toggleStrike()
+            .run()
+        }
+        className={editor.isActive({ textAlign: 'center' }) ? 'is-active' : ''}
+        content={<BiAlignMiddle size="28" />}
+      />
+
+      <WeeklyEditorMenuBarButton
+        onClick={() => editor.chain().focus().setTextAlign('right').run()}
+        disabled={
+          !editor.can().chain().focus()
+            .toggleStrike()
+            .run()
+        }
+        className={editor.isActive({ textAlign: 'right' }) ? 'is-active' : ''}
+        content={<BiAlignRight size="28" />}
+      />
+      <input
+        type="color"
+        onInput={(event) => editor.chain().focus().setColor(event.target.value).run()}
+        // value={editor.getAttributes('textStyle').color}
+        value="#000000"
+        className="outline-none focus:outline-none border-r border-gray-200 flex-1 justify-center items-center h-10 hover:text-indigo-500 active:bg-gray-50"
+      />
+
+    </div>
+  );
+};
+WeeklyEditorMenuBar.propTypes = {
+  editor: PropTypes.instanceOf(Editor),
+};
+WeeklyEditorMenuBar.defaultProps = {
+  editor: {},
+};
+export default WeeklyEditorMenuBar;
