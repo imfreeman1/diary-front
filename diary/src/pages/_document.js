fc767a514324056fc1e40d70827eee276b8a9834
imfreeman1 <dkfrhkt@gmail.com>
2023-05-18 19:36:43 +0900

    _document 수정
     - cache 관리를 위한 meta tag 추가

--- diary/src/pages/_document.js
+++ diary/src/pages/_document.js
@@ -1,11 +1,22 @@
-import {
-  Html, Head, Main, NextScript,
-} from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="kr">
+      <Head>
+        <meta
+          httpEquiv="Cache-Control"
+          content="no-cache, no-store, must-revalidate"
+        />
+        <meta
+          httpEquiv="Pragma"
+          content="no-cache"
+        />
+        <meta
+          httpEquiv="Expires"
+          content="0"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
