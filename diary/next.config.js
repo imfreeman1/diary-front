19744df97d4d6b16ece9bd38f948433f12ddef2d
imfreeman1 <dkfrhkt@gmail.com>
2023-05-19 16:29:42 +0900

    next.config 수정
     - image 태그에서 허용하는 외부 도메인 설정
     - svg를 받아올 수 있도록 허가.

--- diary/next.config.js
+++ diary/next.config.js
@@ -11,7 +11,9 @@ module.exports = nextConfig;
 // https:// nextjs.org/docs/pages/api-reference/next-config-js/images
 module.exports = {
   images: {
-    loader: 'akamai',
-    path: '/',
+    dangerouslyAllowSVG: true, // svg를 허용하는 config로, 추후 svg를 허용하지 않을경우 삭제하는 것이 좋다.
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    domains: ['mydiary-iamges.s3.ap-northeast-2.amazonaws.com'],
   },
 };
