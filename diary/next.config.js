6146b3162a482deb5fc53058de98593ca675afa8
imfreeman1 <dkfrhkt@gmail.com>
2023-05-19 17:39:13 +0900

    next.config 수정
     - config 나누어 져있던것 통합,
     - next export를 config에서 output export로 변경

--- diary/next.config.js
+++ diary/next.config.js
@@ -4,16 +4,15 @@ const nextConfig = {
   env: {
     GOOGLE_KEY: process.env.REACT_APP_GOOGLE_API_KEY,
   },
-};
-
-module.exports = nextConfig;
-
-// https:// nextjs.org/docs/pages/api-reference/next-config-js/images
-module.exports = {
   images: {
     dangerouslyAllowSVG: true, // svg를 허용하는 config로, 추후 svg를 허용하지 않을경우 삭제하는 것이 좋다.
     contentDispositionType: 'attachment',
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     domains: ['mydiary-iamges.s3.ap-northeast-2.amazonaws.com'],
   },
+  output: 'export',
 };
+
+module.exports = nextConfig;
+
+// https:// nextjs.org/docs/pages/api-reference/next-config-js/images
