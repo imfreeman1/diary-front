66fa6ca79a1b4eaa2b89ebfa63c1717d4c2cee40
imfreeman1 <dkfrhkt@gmail.com>
2023-05-19 17:36:00 +0900

    Profile 수정
     - propTypes에 imgRef 추가

--- diary/src/Components/Profile/ProfilePresent.jsx
+++ diary/src/Components/Profile/ProfilePresent.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/forbid-prop-types */
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from '../Button';
@@ -60,6 +61,8 @@ const ProfilePresent = ({ onChange, imgRef }) => (
 
 ProfilePresent.propTypes = {
   onChange: PropTypes.func.isRequired,
+  imgRef: PropTypes.shape({ current: PropTypes.object }).isRequired,
+
 };
 
 export default ProfilePresent;
