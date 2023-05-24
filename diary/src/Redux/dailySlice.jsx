1697d1b841451543fc6d1e12a999b336b28fc2ab
kimud6003 <kimud6003@gmail.com>
2023-05-24 23:23:03 +0900

    - HotFix Daily 리팩토링

--- diary/src/Redux/dailySlice.jsx
+++ diary/src/Redux/dailySlice.jsx
@@ -1,34 +1,42 @@
 /* eslint-disable no-param-reassign */
 import { createSlice } from '@reduxjs/toolkit';
-
-const calNAME = 'editorContent';
+import { DAILY_CALENDAR_NAME } from './sliceName';
+import { DAILY_CONST } from 'src/Constants/dailyConstant';
 
 export const dailySlice = createSlice({
-  name: calNAME,
+  name: DAILY_CALENDAR_NAME,
   initialState: {
-    dailyContents: {},
+    dailyContents: {
+      currentDate :  '0000-00-00',
+      default: {
+        locdate: '0000-00-00',
+        day: '일',
+        titleText: '',
+        editorContent: '',
+      }
+    },
   },
   reducers: {
     setDaily: ({ dailyContents }, { payload, payload: { locdate } }) => {
-      if (!dailyContents[`D-${locdate}`]) {
-        dailyContents[`D-${locdate}`] = payload;
+      if (!dailyContents[DAILY_CONST.MARK(locdate)]) {
+        dailyContents[DAILY_CONST.MARK(locdate)] = payload;
       }
     },
     setDate: ({ dailyContents }, { payload }) => {
       dailyContents.currentDate = payload;
     },
     setEditor: ({ dailyContents }, { payload: { locdate, editorContent } }) => {
-      dailyContents[`D-${locdate}`].editorContent = editorContent;
+      dailyContents[DAILY_CONST.MARK(locdate)].editorContent = editorContent;
     },
     setTitle: ({ dailyContents }, { payload: { locdate, titleText } }) => {
-      if (dailyContents[`D-${locdate}`]) {
-        dailyContents[`D-${locdate}`].titleText = titleText;
+      if (dailyContents[DAILY_CONST.MARK(locdate)]) {
+        dailyContents[DAILY_CONST.MARK(locdate)].titleText = titleText;
       }
     },
     setSave: ({ dailyContents }, { payload: { locdate, titleText, editorContent } }) => {
-      if (dailyContents[`D-${locdate}`]) {
-        dailyContents[`D-${locdate}`].titleText = titleText;
-        dailyContents[`D-${locdate}`].editorContent = editorContent;
+      if (dailyContents[DAILY_CONST.MARK(locdate)]) {
+        dailyContents[DAILY_CONST.MARK(locdate)].titleText = titleText;
+        dailyContents[DAILY_CONST.MARK(locdate)].editorContent = editorContent;
       }
     },
   },
