96bf5f24f146db7d30311a37999029e15ccf1f44
kimud6003 <kimud6003@gmail.com>
2023-05-24 21:39:57 +0900

    - HotFix TemporaryStorage 삭제
    (cherry picked from commit 0e5ccd0d0f89c4579f59ac1a588edb3594a71f45)

--- diary/temporaryStorage/SignupInput/SignupInputPresent.jsx
+++
@@ -1,8 +0,0 @@
-// import React from 'react';
-// import PropTypes from 'prop-types';
-
-// const SignupInputPresent = (props) => <div>SignupInputPresent</div>;
-
-// SignupInputPresent.propTypes = {};
-
-// export default SignupInputPresent;
