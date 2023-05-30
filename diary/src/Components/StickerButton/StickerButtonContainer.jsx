739429b35cdb24d59b2db0c851c4e496764d7f3f
kimud6003 <kimud6003@gmail.com>
2023-05-30 22:24:09 +0900

    - HotFix StikcerButtonContainer 리팩토링

--- diary/src/Components/StickerButton/StickerButtonContainer.jsx
+++ diary/src/Components/StickerButton/StickerButtonContainer.jsx
@@ -7,34 +7,27 @@ import urlToFile from 'src/Utils/urlToFile';
 import makeFormData from 'src/Utils/makeFormData';
 import utilAxios from 'src/Utils/utilAxios';
 import { v4 } from 'uuid';
+import { SET_STICKER_OPTIONS, STICKER_FORM_DATA_OBJ } from 'src/Constants/stickerConstant';
 import StickerButtonPresent from './StickerButtonPresent';
 
 function StickerButtonContainer({ sticker, pageDate }) {
   const dispatch = useDispatch();
   const currRouter = CURRENT_ROUTER_PATH();
-  const makeStickerHandler = async (e) => {
+  const makeStickerHandler = async ({ view: { innerHeight, innerWidth } }) => {
     const newId = v4();
-    const positionX = e.view.innerWidth / 2;
-    const positionY = e.view.innerHeight / 4;
-    const imgFile = await urlToFile(sticker.imgURL, 'image');
-    const stickerFormDataObject = {
-      id: newId,
-      page_date: pageDate,
-      page_type: currRouter.toLowerCase(),
-      image_name: '이름',
-      image: imgFile,
-      'size[0]': sticker.height,
-      'size[1]': sticker.width,
-      'position[0]': positionX,
-      'position[1]': positionY,
-    };
+    const [positionX, positionY] = [innerWidth / 2, innerHeight / 4];
+    const imgFile = await urlToFile(sticker, 'image');
+    const stickerFormDataObject = STICKER_FORM_DATA_OBJ(
+      newId,
+      pageDate,
+      currRouter,
+      imgFile,
+      sticker,
+      positionX,
+      positionY,
+    );
     const formData = makeFormData(stickerFormDataObject);
-    const setStickerOptions = {
-      url: '/sticker/write',
-      method: 'post',
-      header: { 'Content-Type': 'multipart/form-data' },
-      data: formData,
-    };
+    const setStickerOptions = SET_STICKER_OPTIONS(formData);
     await utilAxios(setStickerOptions);
     dispatch(setSticker(
       {
