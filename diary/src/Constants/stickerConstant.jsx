b12a15093c98a2beebbd7b81c03f0bb39f3c7ed1
kimud6003 <kimud6003@gmail.com>
2023-05-30 22:24:55 +0900

    - StickerConstant 추가

--- diary/src/Constants/stickerConstant.jsx
+++ diary/src/Constants/stickerConstant.jsx
@@ -17,6 +17,12 @@ export const STICKER_DATA = (id, position, width, height, routerRef) => {
   };
 };
 
+export const GET_STICKER_OPTIONS = (currRouter, pageDate) => ({
+  url: `/sticker/read${currRouter.toLowerCase()}/${pageDate}`,
+  method: 'get',
+  getReturn: true,
+});
+
 export const UPDATE_STICKER_OPTIONS = (data) => ({
   url: '/sticker/update',
   method: 'post',
@@ -28,4 +34,31 @@ export const REMOVE_STICKER_OPTIONS = (data) => ({
   method: 'post',
   data,
   getReturn: true,
+});
+
+export const SET_STICKER_OPTIONS = (data) => ({
+  url: '/sticker/write',
+  method: 'post',
+  header: { 'Content-Type': 'multipart/form-data' },
+  data,
+});
+
+export const STICKER_FORM_DATA_OBJ = (
+  id,
+  pageDate,
+  currRouter,
+  imgFile,
+  sticker,
+  positionX,
+  positionY,
+) => ({
+  id,
+  page_date: pageDate,
+  page_type: currRouter.toLowerCase(),
+  image_name: '이름',
+  image: imgFile,
+  'size[0]': sticker.height,
+  'size[1]': sticker.width,
+  'position[0]': positionX,
+  'position[1]': positionY,
 });
