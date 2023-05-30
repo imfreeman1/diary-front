9f217436237254a4004ee5565183d1079366b53b
kimud6003 <kimud6003@gmail.com>
2023-05-30 21:43:18 +0900

    - HotFix useResizeable 리팩토링

--- diary/src/hooks/useResizable.jsx
+++ diary/src/hooks/useResizable.jsx
@@ -3,73 +3,67 @@ import { useLayoutEffect, useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import { setResize } from 'src/Redux/action';
 import debounce from 'src/Utils/debounce';
-import {
-  CURRENT_ROUTER_PATH,
-} from '../Constants/constants';
 import { STICKER_CONST } from 'src/Constants/stickerConstant';
+import interactConfig from 'src/Config/interactConfig';
+import { CURRENT_ROUTER_PATH } from '../Constants/constants';
+
+const resizableStyleApply = (style, width, height, x, y) => {
+  Object.assign(style, STICKER_CONST.IMG_SIZE_OBJECT(width, height, x, y));
+};
 
 const useResizable = (pageDate, focusRef, id) => {
+  const currRouter = CURRENT_ROUTER_PATH();
   const stickerSize = useRef(null);
   const dispatch = useDispatch();
-  const currRouter = useRef(null);
-  currRouter.current = CURRENT_ROUTER_PATH();
 
   useLayoutEffect(() => {
-    interact('.resizable').resizable({
-      edges: {
-        top: true,
-        left: true,
-        bottom: true,
-        right: true,
-      },
-      // invert: 'reposition',
-      listeners: {
-        move: (event) => {
-          let { x, y } = event.target.dataset;
-          x = (parseFloat(x) || 0) + event.deltaRect.left;
-          y = (parseFloat(y) || 0) + event.deltaRect.top;
-          stickerSize.current = {
-            width: event.rect.width,
-            height: event.rect.height,
-          };
-          // 여기 부분에서 debounce가 아닌 mouseUp될때 dispatch를 실행할 수 있도록 변경해야하 할 것 같음. draggable에서도 동일.
-          Object.assign(
-            event.target.style,
-            STICKER_CONST.IMG_SIZE_OBJECT(
-              stickerSize.current.width,
-              stickerSize.current.height,
-              x,
-              y,
-            ),
-          );
-          /* move event가 발생하는 동안 event.target.dataset을 실시간 변경해줌. */
-          Object.assign(event.target.dataset, { x, y });
-          const callBackDispatch = () => {
-            dispatch(
-              setResize({
-                origin: currRouter.current,
-                id,
-                size: stickerSize.current,
-                position: { x, y },
-                pageDate,
-              }),
-            );
-            Object.assign(
-              event.target.style,
-              STICKER_CONST.IMG_SIZE_OBJECT(
-                stickerSize.current.width,
-                stickerSize.current.height,
-                0,
-                0,
-              ),
-            );
-          };
-          debounce(300, callBackDispatch);
-        },
-      },
-    });
+    const stickerResizeMoveMethod = ({ target, deltaRect, rect }) => {
+      let { x, y } = target.dataset;
+      x = parseFloat(x) + deltaRect.left || deltaRect.left;
+      y = parseFloat(y) + deltaRect.top || deltaRect.top;
+
+      stickerSize.current = {
+        width: rect.width,
+        height: rect.height,
+      };
+      // 여기 부분에서 debounce가 아닌 mouseUp될때 dispatch를 실행할 수 있도록 변경해야하 할 것 같음. draggable에서도 동일.
+      resizableStyleApply(
+        target.style,
+        stickerSize.current.width,
+        stickerSize.current.height,
+        x,
+        y,
+      );
+
+      /* move event가 발생하는 동안 event.target.dataset을 실시간 변경해줌. */
+      Object.assign(target.dataset, { x, y });
+
+      const callBackDispatch = () => {
+        dispatch(
+          setResize({
+            origin: currRouter,
+            id,
+            size: stickerSize.current,
+            position: { x, y },
+            pageDate,
+          }),
+        );
+        resizableStyleApply(
+          target.style,
+          stickerSize.current.width,
+          stickerSize.current.height,
+          0,
+          0,
+        );
+      };
+      debounce(300, callBackDispatch);
+    };
+
+    interact('.resizable').resizable(
+      interactConfig.resizable(stickerResizeMoveMethod),
+    );
     return () => clearTimeout(debounce);
-  }, [dispatch]);
+  }, [currRouter, dispatch, id, pageDate]);
 };
 
 export default useResizable;
