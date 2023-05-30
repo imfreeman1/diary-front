8e3eb734529f09a3e47284114543b875c74d3795
kimud6003 <kimud6003@gmail.com>
2023-05-30 21:42:40 +0900

    - HotFix Update,Remove  옵션 생성

--- diary/src/Constants/stickerConstant.jsx
+++ diary/src/Constants/stickerConstant.jsx
@@ -1,9 +1,31 @@
-export const STICKER_CONST ={
-  IMG_SIZE_OBJECT : (width, height, x, y) => ({
+export const STICKER_CONST = {
+  IMG_SIZE_OBJECT: (width, height, x, y) => ({
     width: `${width}px`,
     height: `${height}px`,
     transform: `translate(${x}px, ${y}px)`,
   }),
-  POSITION_TRANSLATOR : (position) => `translate(${position.positionX}px, ${position.positionY}px)`
-}
+  POSITION_TRANSLATOR: (position) => `translate(${position.positionX}px, ${position.positionY}px)`,
+};
 
+export const STICKER_DATA = (id, position, width, height, routerRef) => {
+  const { positionX, positionY } = position;
+  return {
+    id,
+    position: [positionX, positionY],
+    size: [height, width],
+    page_type: routerRef?.toLowerCase(),
+  };
+};
+
+export const UPDATE_STICKER_OPTIONS = (data) => ({
+  url: '/sticker/update',
+  method: 'post',
+  data,
+});
+
+export const REMOVE_STICKER_OPTIONS = (data) => ({
+  url: '/sticker/delete',
+  method: 'post',
+  data,
+  getReturn: true,
+});
