aae22be757f43be53404a360e8ac169fcb042deb
kimud6003 <kimud6003@gmail.com>
2023-06-01 17:16:18 +0900

    - HotFix DailyConstant REST_API상수추가

--- diary/src/Constants/dailyConstant.jsx
+++ diary/src/Constants/dailyConstant.jsx
@@ -1,6 +1,31 @@
-export const DAILY_CONST ={
+export const DAILY_CONST = {
   LOGO: 'Daily',
   TITLE: 'Title',
-}
+  MARK: (date) => `D-${date}`,
+};
 
 // export const DAILY_TITLE = 'Title';
+export const GET_DAILY_DIARY_OPT = (date) => ({
+  method: 'get',
+  url: `/daily/read/${date}`,
+});
+
+export const POST_DAILY_WRITE_OPT = (dailyInfo) => ({
+  method: 'post',
+  url: '/daily/write/',
+  payload: {
+    title: dailyInfo.titleText,
+    content: dailyInfo.editorContent,
+    date: dailyInfo.locdate,
+  },
+});
+
+export const POST_DAILY_UPDATE_OPT = (dailyInfo) => ({
+  method: 'post',
+  url: '/daily/update',
+  payload: {
+    title: dailyInfo.titleText,
+    content: dailyInfo.editorContent,
+    date: dailyInfo.locdate,
+  },
+});
