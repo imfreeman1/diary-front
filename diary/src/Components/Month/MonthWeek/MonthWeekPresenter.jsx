06e8dcdd7f3dc9ad36cdf96590de1cb9ca1bbead
kimud6003 <kimud6003@gmail.com>
2023-06-02 21:17:37 +0900

    - HotFix MonthWeek 리팩토링 누락 수정

--- diary/src/Components/Month/MonthWeek/MonthWeekPresenter.jsx
+++ diary/src/Components/Month/MonthWeek/MonthWeekPresenter.jsx
@@ -8,28 +8,38 @@ import MonthDateContainer from '../MonthDate/MonthDateContainer';
  * @returns
  */
 
-const MonthWeekPresenter = ({ week }) => (
-  <tbody>
-    <tr className="flex flex-grow m-2 gap-3">
-      {week.map((dayInfo) => <MonthDateContainer dayInfo={dayInfo} key={v4()} />)}
-    </tr>
-  </tbody>
-);
+const MonthWeekPresenter = ({ week }) => {
+  const weekArray = [0, 1, 2, 3, 4, 5, 6];
+  return (
+    <tbody>
+      <tr className="flex flex-grow m-2 gap-3">
+        {weekArray.map((dayIdx) => (
+          <MonthDateContainer
+            dayIdx={dayIdx}
+            week={week}
+            key={v4()}
+          />
+        ))}
+      </tr>
+    </tbody>
+  );
+};
 
 MonthWeekPresenter.propTypes = {
-  week: PropTypes.arrayOf(PropTypes.shape({
-    date: PropTypes.number.isRequired,
-    dateName: PropTypes.string.isRequired,
-    day: PropTypes.string.isRequired,
-    isHoliday: PropTypes.bool.isRequired,
-    isInMonth: PropTypes.bool.isRequired,
-    locdate: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]).isRequired,
-    todos: PropTypes.arrayOf(PropTypes.shape({
-      date: PropTypes.string.isRequired,
-      id: PropTypes.string.isRequired,
-      todoContent: PropTypes.string.isRequired,
-    })).isRequired,
-  }).isRequired).isRequired,
+  // week: PropTypes.arrayOf(PropTypes.shape({
+  //   date: PropTypes.number.isRequired,
+  //   dateName: PropTypes.string.isRequired,
+  //   day: PropTypes.string.isRequired,
+  //   isHoliday: PropTypes.bool.isRequired,
+  //   isInMonth: PropTypes.bool.isRequired,
+  //   locdate: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]).isRequired,
+  //   todos: PropTypes.arrayOf(PropTypes.shape({
+  //     date: PropTypes.string.isRequired,
+  //     id: PropTypes.string.isRequired,
+  //     todoContent: PropTypes.string.isRequired,
+  //   })).isRequired,
+  // }).isRequired).isRequired,
+  week: PropTypes.number.isRequired,
 
 };
 
