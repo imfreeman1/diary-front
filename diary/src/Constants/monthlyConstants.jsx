ba15485290d3e90406beac87f397610fb0dd4138
kimud6003 <kimud6003@gmail.com>
2023-06-02 21:16:46 +0900

    - HotFix MonthConstant 추가

--- diary/src/Constants/monthlyConstants.jsx
+++ diary/src/Constants/monthlyConstants.jsx
@@ -7,6 +7,34 @@ export const MONTH_CONST = {
   BASIC_SHOW_TODO: 2,
   MAX_SHOW_TODO: 3,
   INPUT_PLACEHOLDER: '입력하세요',
-  SHOW_MORE_TODO: (todos) => `일정 ${todos.length - this.BASIC_SHOW_TODO}개 더보기`,
-
+  SHOW_MORE_TODO: (todos) => `일정 ${todos.length - MONTH_CONST.BASIC_SHOW_TODO}개 더보기`,
 };
+
+export const GET_MONTH_READ_OPT = (monthDate) => ({
+  method: 'GET',
+  url: `/monthly/read/${monthDate}`,
+});
+
+export const POST_MONTH_WRITE_OPT = (inputText, locdate) => ({
+  method: 'post',
+  url: '/monthly/write/',
+  payload: {
+    content: [inputText],
+    date: locdate,
+  },
+});
+
+export const POST_MONTH_UPDATE_OPT = (todos, inputText, locdate) => ({
+  method: 'post',
+  url: '/monthly/update',
+  payload: {
+    content: [...todos.map((item) => item.todoContent), inputText],
+    date: locdate,
+  },
+});
+
+export const POST_MONTH_DELETE_OPT = (locdate) => ({
+  method: 'post',
+  url: '/monthly/delete',
+  payload: { date: locdate },
+});
