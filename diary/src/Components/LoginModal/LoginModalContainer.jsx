3cc5240b6baa3ef093d54e93fa854007e34df00d
kimud6003 <kimud6003@gmail.com>
2023-06-05 21:44:41 +0900

    - Login 파일명 변경

--- diary/src/Components/LoginModal/LoginModalContainer.jsx
+++ diary/src/Components/LoginModal/LoginModalContainer.jsx
@@ -1,89 +1,58 @@
 import React from 'react';
-import { LOGIN } from 'src/Constants/constants';
+import { useForm } from 'react-hook-form';
+import { useRouter } from 'next/router';
+import axios from 'src/Utils/api';
 import PropTypes from 'prop-types';
+import { LOGIN_EMAIL, LOGIN_PWD } from 'src/Constants/LoginConstant';
+import LoginModalPresenter from './LoginModalPresenter';
 
-const LoginModalContainer = ({
-  handleLogin, emailRegister, passwordRegister, isSubmitting, setIsSignup,
-}) => (
-  <div className="flex flex-col justify-center gap-1">
-    <p className="text-2xl mb-2 font-bold text-center">{LOGIN.CONTENT}</p>
-    <form onSubmit={handleLogin}>
-      <div className="flex justify-center m-3">
-        <div className="flex flex-col justify-around">
-          <p className="px-3">{LOGIN.FORM_EMAIL}</p>
-          <p className="px-3">{LOGIN.FORM_PASSWORD}</p>
-        </div>
-        <div className="flex flex-col justify-between gap-1">
-          <input
-            id="email"
-            type="email"
-            placeholder="test@email.com"
-            className="border-2 rounded-md h-10 px-3"
-            {...emailRegister}
-          />
-          <input
-            id="password"
-            type="password"
-            placeholder="****************"
-            className="border-2 rounded-md h-10 px-3"
-            {...passwordRegister}
-          />
-        </div>
-      </div>
-      <div className="m-6">
-        <button
-          className="w-full bg-orange-300 text-white rounded-md p-2.5"
-          type="submit"
-          disabled={isSubmitting}
-        >
-          {LOGIN.CONTENT_KR}
-        </button>
-        <div className="text-sm text-center mt-2">
-          <span className="text-gray-700">{LOGIN.EXPLAIN_SIGNUP}</span>
-          <span
-            onClick={() => setIsSignup(true)}
-            aria-hidden="true"
-            className="w-fit text-blue-900 cursor-pointer hover:underline"
-          >
-            {LOGIN.GO_SIGNUP}
-          </span>
-        </div>
-        <div className="text-sm text-center mt-2">
-          <span className="text-gray-700">{LOGIN.EXPLAIN_LOSS_PASSWORD}</span>
-          <span
-            onClick={() => setIsSignup(true)}
-            aria-hidden="true"
-            className="w-fit text-blue-900 cursor-pointer hover:underline"
-          >
-            {LOGIN.FIND_PASSWORD}
-          </span>
-        </div>
-      </div>
-      <div />
-    </form>
-    <button
-      type="button"
-      className="bg-gray-300 p-2 px-10 rounded-xl"
-    >
-      {LOGIN.GOOGLE_SIMPLE}
-    </button>
-  </div>
-);
+const LoginModalContainer = ({ setIsSignup }) => {
+  const router = useRouter();
+  const googleUrl = ((router.state)?.from?.pathname) || '/';
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm();
+
+  const passwordRegister = register(LOGIN_PWD.name, LOGIN_PWD.option);
+  const emailRegister = register(LOGIN_EMAIL.name, LOGIN_EMAIL.option);
+
+  const handleLogin = handleSubmit(async (resData) => {
+    try {
+      const response = await axios.post(
+        '/users/signin',
+        {
+          email: resData.email,
+          password: resData.password,
+          name: '',
+          image: '',
+        },
+        {
+          withCredentials: true,
+        },
+      );
+      if (response.data.code === 'USI20001') {
+        router.push('/Cover');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  return (
+    <LoginModalPresenter
+      handleLogin={handleLogin}
+      isSubmitting={isSubmitting}
+      setIsSignup={setIsSignup}
+      passwordRegister={passwordRegister}
+      emailRegister={emailRegister}
+      register={register}
+      googleUrl={googleUrl}
+    />
+  );
+};
 LoginModalContainer.propTypes = {
-  handleLogin: PropTypes.func.isRequired,
-  passwordRegister: PropTypes.shape({
-    name: PropTypes.string,
-    onChange: PropTypes.func,
-    onBlur: PropTypes.func,
-    ref: PropTypes.func,
-  }).isRequired,
-  emailRegister: PropTypes.shape({
-    name: PropTypes.string,
-    onChange: PropTypes.func,
-    onBlur: PropTypes.func,
-    ref: PropTypes.func,
-  }).isRequired,
-  isSubmitting: PropTypes.bool.isRequired,
   setIsSignup: PropTypes.func.isRequired,
 };
 export default LoginModalContainer;
