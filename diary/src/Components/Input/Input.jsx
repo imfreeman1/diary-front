cc6f5f180005ae9ad933e8c1d71ec3cf231544ea
imfreeman1 <dkfrhkt@gmail.com>
2023-07-09 23:18:15 +0900

    Input porpTypes 작성

--- diary/src/Components/Input/Input.jsx
+++ diary/src/Components/Input/Input.jsx
@@ -1,15 +1,33 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-const Input = ({placeholder, value, onChange, className, required, inputRef}) => {
+const Input = ({
+  placeholder, value, onChange, className, required, inputRef,
+}) => {
   console.log('in Input', inputRef);
   return (
-    <input placeholder={placeholder}
-    value={value}
-    ref={inputRef}
-    onChange={onChange}
-    className={className}
-    required={required}/>
-  )
-}
+    <input
+      placeholder={placeholder}
+      value={value}
+      ref={inputRef}
+      onChange={onChange}
+      className={className}
+      required={required}
+    />
+  );
+};
+Input.defaultProps = {
+  required: false,
+  className: '',
+};
 
+Input.propTypes = {
+  placeholder: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  className: PropTypes.string,
+  required: PropTypes.bool,
+  // eslint-disable-next-line react/forbid-prop-types
+  inputRef: PropTypes.object.isRequired,
+};
 export default React.memo(Input);
